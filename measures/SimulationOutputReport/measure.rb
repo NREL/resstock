4fbd51ef31da69f266d596a352705fd36d00cfc2
Scott Horowitz <scott.horowitz@nrel.gov>
2016-08-06 11:43:52 -0600

    Simplified simulation output reporting measure. Fixed reporting of oil/propane energy consumption. Added hours heating/cooling setpoints not met to output.

--- measures/SimulationOutputReport/measure.rb
+++ measures/SimulationOutputReport/measure.rb
@@ -8,7 +8,7 @@ class SimulationOutputReport < OpenStudio::Ruleset::ReportingUserScript
   end
   
   def description
-    return "Reports fuel and end use simulation outputs."
+    return "Reports simulation outputs of interest."
   end
 
   #define the arguments that the user will input
@@ -43,95 +43,63 @@ class SimulationOutputReport < OpenStudio::Ruleset::ReportingUserScript
     end
     sqlFile = sqlFile.get
     model.setSqlFile(sqlFile)
-
-    fuel_type = ""
-    units = ""
-
-    map = {"site_energy_use"=>"Total Site Energy MBtu",
-           "electricity"=>"Total Site Electricity kWh",
-           "natural_gas"=>"Total Site Natural Gas therm",
-           "additional_fuel"=>"Total Site Other Fuel MBtu",
-           "electricity_heating"=>"Electricity Heating kWh",
-           "electricity_cooling"=>"Electricity Cooling kWh",
-           "electricity_interior_lighting"=>"Electricity Interior Lighting kWh",
-           "electricity_exterior_lighting"=>"Electricity Exterior Lighting kWh",
-           "electricity_interior_equipment"=>"Electricity Interior Equipment kWh",
-           "electricity_fans"=>"Electricity Fans kWh",
-           "electricity_pumps"=>"Electricity Pumps kWh",
-           "electricity_water_systems"=>"Electricity Water Systems kWh",
-           "natural_gas_heating"=>"Natural Gas Heating therm",
-           "natural_gas_interior_equipment"=>"Natural Gas Interior Equipment therm",
-           "natural_gas_water_systems"=>"Natural Gas Water Systems therm",
-           "additional_fuel_heating"=>"Other Fuel Heating MBtu",
-           "additional_fuel_interior_equipment"=>"Other Fuel Interior Equipment MBtu",
-           "additional_fuel_water_systems"=>"Other Fuel Water Systems MBtu"}
     
-    site_energy_use = 0.0
-    OpenStudio::EndUseFuelType::getValues.each do |fuel_type|
-      fuel_type = OpenStudio::EndUseFuelType.new(fuel_type).valueDescription
-      if fuel_type == "Electricity"
-        units = "\"kWh\""
-        unit_str = "kWh"
-      elsif fuel_type == "Natural Gas"
-        units = "\"therm\""
-        unit_str = "therm"
-      else
-        units = "\"Million Btu\""
-        unit_str = "MBtu"
-      end
-      fuel_type_aggregation = 0.0
-      OpenStudio::EndUseCategoryType::getValues.each do |category_type|
-        fuel_and_category_aggregation = 0.0
-        category_str = OpenStudio::EndUseCategoryType.new(category_type).valueDescription
-        OpenStudio::MonthOfYear::getValues.each do |month|
-          if month >= 1 and month <= 12
-            if not sqlFile.energyConsumptionByMonth(OpenStudio::EndUseFuelType.new(fuel_type),
-                                                    OpenStudio::EndUseCategoryType.new(category_type),
-                                                    OpenStudio::MonthOfYear.new(month)).empty?
-              valInJ = sqlFile.energyConsumptionByMonth(OpenStudio::EndUseFuelType.new(fuel_type),
-                                                        OpenStudio::EndUseCategoryType.new(category_type),
-                                                        OpenStudio::MonthOfYear.new(month)).get
-              fuel_and_category_aggregation += valInJ
-              month_str = OpenStudio::MonthOfYear.new(month).valueDescription
-              name = OpenStudio::toUnderscoreCase("#{fuel_type}_#{category_str}_#{month_str}")
-              valInUnits = OpenStudio::convert(valInJ,"J",unit_str).get()
-              #runner.registerValue(name,valInUnits,unit_str)
-              #runner.registerInfo("Registered: #{name},#{valInUnits},#{unit_str}")
-            end
-          end
-        end
-        name = get_mapped_name(OpenStudio::toUnderscoreCase("#{fuel_type}_#{category_str}"), map)
-        valInUnits = OpenStudio::convert(fuel_and_category_aggregation,"J",unit_str).get
-        runner.registerValue(name,valInUnits,unit_str)
-        runner.registerInfo("Registered: #{name},#{valInUnits},#{unit_str}")
-        fuel_type_aggregation += fuel_and_category_aggregation
-      end
-      name = get_mapped_name(OpenStudio::toUnderscoreCase("#{fuel_type}"), map)
-      valInUnits = OpenStudio::convert(fuel_type_aggregation,"J",unit_str).get
-      runner.registerValue(name,valInUnits,unit_str)
-      runner.registerInfo("Registered: #{name},#{valInUnits},#{unit_str}")
-      site_energy_use += fuel_type_aggregation
-    end
-    name = get_mapped_name("site_energy_use", map)
-    valInUnits = OpenStudio::convert(site_energy_use,"J","MBtu").get
-    runner.registerValue(name,valInUnits,"MBtu")
-    runner.registerInfo("Registered: #{name},#{valInUnits},MBtu")
-
+    elec_site_units = "kWh"
+    gas_site_units = "therm"
+    other_fuel_site_units = "MBtu"
+           
+    # Total
+    report_output(runner, "Total Site Energy", sqlFile.totalSiteEnergy, "GJ", "MBtu")
+    
+    # Electricity
+    report_output(runner, "Total Site Electricity", sqlFile.electricityTotalEndUses, "GJ", elec_site_units)
+    report_output(runner, "Electricity Heating", sqlFile.electricityHeating, "GJ", elec_site_units)
+    report_output(runner, "Electricity Cooling", sqlFile.electricityCooling, "GJ", elec_site_units)
+    report_output(runner, "Electricity Interior Lighting", sqlFile.electricityInteriorLighting, "GJ", elec_site_units)
+    report_output(runner, "Electricity Exterior Lighting", sqlFile.electricityExteriorLighting, "GJ", elec_site_units)
+    report_output(runner, "Electricity Interior Equipment", sqlFile.electricityInteriorEquipment, "GJ", elec_site_units)
+    report_output(runner, "Electricity Fans", sqlFile.electricityFans, "GJ", elec_site_units)
+    report_output(runner, "Electricity Pumps", sqlFile.electricityPumps, "GJ", elec_site_units)
+    report_output(runner, "Electricity Water Systems", sqlFile.electricityWaterSystems, "GJ", elec_site_units)
+    
+    # Natural Gas
+    report_output(runner, "Total Site Natural Gas", sqlFile.naturalGasTotalEndUses, "GJ", gas_site_units)
+    report_output(runner, "Natural Gas Heating", sqlFile.naturalGasHeating, "GJ", gas_site_units)
+    report_output(runner, "Natural Gas Interior Equipment", sqlFile.naturalGasInteriorEquipment, "GJ", gas_site_units)
+    report_output(runner, "Natural Gas Water Systems", sqlFile.naturalGasWaterSystems, "GJ", gas_site_units)
+    
+    # Other Fuel
+    report_output(runner, "Total Site Other Fuel", sqlFile.otherFuelTotalEndUses, "GJ", other_fuel_site_units)
+    report_output(runner, "Other Fuel Heating", sqlFile.otherFuelHeating, "GJ", other_fuel_site_units)
+    report_output(runner, "Other Fuel Interior Equipment", sqlFile.otherFuelInteriorEquipment, "GJ", other_fuel_site_units)
+    report_output(runner, "Other Fuel Water Systems", sqlFile.otherFuelWaterSystems, "GJ", other_fuel_site_units)
+    
+    # Other Outputs
+    report_output(runner, "Hours Heating Setpoint Not Met", sqlFile.hoursHeatingSetpointNotMet, nil, nil)
+    report_output(runner, "Hours Cooling Setpoint Not Met", sqlFile.hoursCoolingSetpointNotMet, nil, nil)
+    
     #closing the sql file
     sqlFile.close()
 
     #reporting final condition
-    runner.registerFinalCondition("Standard Report generated successfully.")
+    runner.registerFinalCondition("Report generated successfully.")
 
     return true
 
   end #end the run method
 
-  def get_mapped_name(name, map)
-    if map.has_key?(name)
-      return map[name]
+  def report_output(runner, name, val, os_units=nil, report_units=nil)
+    return if val.empty?
+    if not report_units.nil?
+        name = "#{name} #{report_units}"
     end
-    return name
+    if os_units.nil? or report_units.nil?
+        valInUnits = val.get
+    else
+        valInUnits = OpenStudio::convert(val.get,os_units,report_units).get
+    end
+    runner.registerValue(name,valInUnits)
+    runner.registerInfo("Registered: #{name}, #{valInUnits.round(2)}")
   end
   
 end #end the measure
