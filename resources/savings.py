955f9320329f7b3ed60f2fd8365116f7aba22473
Joe Robertson <joseph.robertson@nrel.gov>
2017-03-02 11:01:40 -0700

    Adding output csv with savings deltas.

--- resources/savings.py
+++ resources/savings.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 from joblib import Parallel, delayed
 import multiprocessing
+import argparse
 
 def main(file):
 
@@ -12,9 +13,8 @@ def main(file):
   full = df.copy()
   
   # get upgrades  
-  upgrades = [col for col in df.columns if 'upgrade' in col]
-  upgrades = [col for col in upgrades if not 'simulation_output_report' in col]
-  
+  upgrades = [col for col in df.columns if col.endswith('.run_measure')]
+    
   # get enduses  
   enduses = [col for col in df.columns if 'simulation_output_report' in col]
   enduses = [col for col in enduses if not '.weight' in col]
@@ -32,7 +32,7 @@ def main(file):
     except:
       print ' ... did not remove {}'.format(col)  
   
-  # clean columns  
+  # clean cost column
   df['simulation_output_report.upgrade_cost'] = df['simulation_output_report.upgrade_cost'].str.strip()
   df['simulation_output_report.upgrade_cost'] = df['simulation_output_report.upgrade_cost'].str.replace('$', '')
   df['simulation_output_report.upgrade_cost'] = df['simulation_output_report.upgrade_cost'].str.replace(',', '')
@@ -57,7 +57,7 @@ def deltas(df, upgrades, enduses):
   df_upgrades = df.loc[df[upgrades].sum(axis=1)!=0]
   
   # incremental cost
-  df.loc[df_upgrades.index, 'incremental_cost'] = df_upgrades['simulation_output_report.upgrade_cost'].values - df_reference['simulation_output_report.upgrade_cost'].values
+  df.loc[df_upgrades.index, 'incremental_cost_usd'] = df_upgrades['simulation_output_report.upgrade_cost'].values - df_reference['simulation_output_report.upgrade_cost'].values
   
   # energy savings
   for enduse in enduses:
@@ -72,8 +72,8 @@ def parallelize(groups, func, upgrades, enduses):
 def identify_upgrade(row, upgrades):
   for upgrade in upgrades:
     if row[upgrade]:
-      return upgrade
-  return np.nan
+      return upgrade.replace('.run_measure', '')
+  return 'reference'
     
 def is_reference_case(x, upgrades):
   upgrades_applied = 0
@@ -81,76 +81,20 @@ def is_reference_case(x, upgrades):
     upgrades_applied += x[upgrade]
   if upgrades_applied == 0:
     return True
-  return False
-      
-def costs(full):
-
-  rates = {}
-  rates['elec'] = pd.read_csv('grid_elec.csv', index_col=['nsrdb_gid_new'])
-  rates['ng'] = pd.read_csv('grid_ng.csv', index_col=['nsrdb_gid_new'])
-  rates['oil'] = pd.read_csv('grid_oil.csv', index_col=['nsrdb_gid_new'])
-  rates['prop'] = pd.read_csv('grid_prop.csv', index_col=['nsrdb_gid_new'])
-  rates = {k: v[v['TotalSFD'] > 0] for k, v in rates.items()}
-  rates = {k: v.rename(columns={'Location': 'state'}) for k, v in rates.items()}
-  for k, v in rates.items():
-    v['usaf'] = v['usaf'].map(str)
-
-  # average rate in each usaf weighted by totalsfd
-  usaf_rates = {}
-  usaf_rates = {k: v.groupby('usaf').agg({'wt_mean_rate': lambda x: np.average(x, weights=rates[k].loc[x.index, 'TotalSFD'])}) for k, v in rates.items()}
-  
-  # average rate in each usaf-state weighted by totalsfd
-  usaf_state_rates = {}
-  usaf_state_rates = {k: v.groupby(['usaf', 'state']).agg({'wt_mean_rate': lambda x: np.average(x, weights=rates[k].loc[x.index, 'TotalSFD'])}) for k, v in rates.items()}
-  
-  full['usaf'] = full['building_characteristics_report.location_epw'].apply(lambda x: x.split('_')[-2].split('.')[-1])
-  full['state'] = full['building_characteristics_report.location_epw'].apply(lambda x: x.split('_')[1])  
-    
-  full['utility_cost_savings'] = full.apply(lambda x: calc_utility_cost(x, usaf_rates['elec'].loc[usaf_rates['elec'].index==x['usaf'], 'wt_mean_rate'].values, usaf_rates['ng'].loc[usaf_rates['ng'].index==x['usaf'], 'wt_mean_rate'].values, usaf_rates['oil'].loc[usaf_rates['oil'].index==x['usaf'], 'wt_mean_rate'].values), axis=1)
-
-  df = full.groupby(['usaf', 'state', 'upgrade']).agg({'savings_simulation_output_report.total_site_electricity_k_wh': np.sum, 'savings_simulation_output_report.total_site_natural_gas_therm': np.sum, 'savings_simulation_output_report.total_site_other_fuel_m_btu': np.sum, 'simulation_output_report.weight': np.sum})
-  
-  usaf_state_rates = {k: df.reset_index().merge(v.reset_index(), on=['usaf', 'state'], how='inner') for k, v in usaf_state_rates.items()} # HERE 726880 has three states
-  for k, v in usaf_state_rates.items():
-    v = v.set_index(['usaf', 'state', 'upgrade'])
-    if k in ['prop']:
-      continue
-    if k == 'elec':
-      v['wt'] = v['savings_simulation_output_report.total_site_electricity_k_wh'] * v['wt_mean_rate'] * v['simulation_output_report.weight']
-      v.reset_index()[['state', 'upgrade', 'savings_simulation_output_report.total_site_electricity_k_wh', 'simulation_output_report.weight', 'wt']].groupby(['state', 'upgrade']).sum().reset_index().set_index('upgrade').sort_index().to_csv('state-{}.csv'.format(k))
-      v[['savings_simulation_output_report.total_site_electricity_k_wh', 'simulation_output_report.weight', 'wt_mean_rate', 'wt']].reset_index().set_index('upgrade').sort_index().to_csv('usaf-state-{}.csv'.format(k))
-    elif k == 'ng':
-      v['wt'] = v['savings_simulation_output_report.total_site_natural_gas_therm'] * v['wt_mean_rate'] * v['simulation_output_report.weight']
-      v.reset_index()[['state', 'upgrade', 'savings_simulation_output_report.total_site_natural_gas_therm', 'simulation_output_report.weight', 'wt']].groupby(['state', 'upgrade']).sum().reset_index().set_index('upgrade').sort_index().to_csv('state-{}.csv'.format(k))
-      v[['savings_simulation_output_report.total_site_natural_gas_therm', 'simulation_output_report.weight', 'wt_mean_rate', 'wt']].reset_index().set_index('upgrade').sort_index().to_csv('usaf-state-{}.csv'.format(k))
-    elif k == 'oil':
-      v['wt'] = v['savings_simulation_output_report.total_site_other_fuel_m_btu'] * v['wt_mean_rate'] * v['simulation_output_report.weight']
-      v.reset_index()[['state', 'upgrade', 'savings_simulation_output_report.total_site_other_fuel_m_btu', 'simulation_output_report.weight', 'wt']].groupby(['state', 'upgrade']).sum().reset_index().set_index('upgrade').sort_index().to_csv('state-{}.csv'.format(k))
-      v[['savings_simulation_output_report.total_site_other_fuel_m_btu', 'simulation_output_report.weight', 'wt_mean_rate', 'wt']].reset_index().set_index('upgrade').sort_index().to_csv('usaf-state-{}.csv'.format(k))    
-
-  return full
-
-def calc_utility_cost(row, elec, ng, oil):
-  cost = row['savings_simulation_output_report.total_site_electricity_k_wh'] * elec + \
-         row['savings_simulation_output_report.total_site_natural_gas_therm'] * ng + \
-         row['savings_simulation_output_report.total_site_other_fuel_m_btu'] * oil
-  try:
-    return cost[0]
-  except:
-    return np.nan      
+  return False  
       
 if __name__ == '__main__':
 
   t0 = time.time()
-
-  file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'analysis_results', 'resstock_pnw.csv')
+  
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--file', default= '../analysis_results/resstock_pnw.csv', help='Relative path of the output csv file.')
+  args = parser.parse_args()
+  file = os.path.abspath(os.path.join(os.path.dirname(__file__), args.file))
 
   df, full = main(file)
-  # full = costs(full)
   
-  # build_exist_models = full[['build_existing_models.building_id', 'building_characteristics_report.location_epw']].drop_duplicates()
-  # usafs_models = build_exist_models.groupby('building_characteristics_report.location_epw').count()  
-  
-  full.to_csv(os.path.basename(file))
+  new_file = '{}_savings{}'.format(os.path.splitext(os.path.basename(file))[0], os.path.splitext(os.path.basename(file))[1])
+  full.to_csv(os.path.join(os.path.dirname(file), new_file))
   
   print "All done! Completed rows in {0:.2f} seconds on".format(time.time()-t0), time.strftime("%Y-%m-%d %H:%M")
