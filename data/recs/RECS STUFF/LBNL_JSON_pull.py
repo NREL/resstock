29639b92929b9e7306984d73785ac9305c7c9344
jalley71 <jalley@mines.edu>
2017-04-06 15:56:12 -0600

    Binned TSV's, and Plots of variables that can be collapsed.

--- data/recs/RECS STUFF/LBNL_JSON_pull.py
+++ data/recs/RECS STUFF/LBNL_JSON_pull.py
@@ -15,7 +15,10 @@ from datetime import timedelta
 import pandas as pd
 import itertools
 import numpy as np
-
+import LBNL_Plot as plot
+from ast import literal_eval
+import random
+import matplotlib as plt
 story_dict = {8: 1,
               16: 2}
 
@@ -52,99 +55,99 @@ def save_to_tsv(g, outfile):
 
 ####### Create Dictionary & Dataframe of all possible values
 
-x = {"Size": [1000, 2000, 3000, 4500], "yearmaderange": [1, 2, 3, 4, 5, 6], "WAP": [0, 1], "EE": [0, 1],
-     "stories": [8, 16], "Region": [1, 2, 3, 4], "Climate_Zone": [1, 2, 3, 4, 5, 6, 7, 8], "Foundation Type": [1, 2, 3],
-     "Duct": [1, 2, 3]}
-df = pd.DataFrame(list(itertools.product(*x.values())), columns=x.keys())
-
-###### Combinations that do not exist on Website
-
-lh = {'Region': [1], 'Climate_Zone': [8]}
-ld = {'Region': [2], 'Climate_Zone': [1, 7]}
-lm = {'Region': [3], 'Climate_Zone': [1, 2, 5, 6, 7]}
-la = {'Region': [4], 'Climate_Zone': [1, 2, 3, 4, 5, 6]}
-lf = [lh, ld, lm, la]
-
-####### Removal of non-existant combinations
-
-for i in range(len(lf)):
-    LF = lf[i]
-    df = df.drop(df[df[list(LF)].isin(LF).all(axis=1)].index.tolist())
-
-df = df.reset_index()
-df = df.drop('index', axis=1)
-df['p_dist'] = ""
-df['c_dist'] = ""
-
-
-####### Create URL and Query
-
-def url(x):
-    for i, row in x.iterrows():
-        url = 'http://resdb.lbl.gov/main.php?step=2&sub=2&run_env_model=&dtype1=&dtype2=&is_ca=&calc_id=2&floor_area=' + str(
-            row['Size']) + '&house_height=' + str(row['stories']) + '&year_built=' + str(
-            row['yearmaderange']) + '&wap=' + str(row['WAP']) + '&ee_home=' + str(row['EE']) + '&region=' + str(
-            row['Region']) + '&zone=' + str(row['Climate_Zone']) + '&foundation=' + str(
-            row['Foundation Type']) + '&duct=' + str(row['Duct'])
-        page = requests.get(url)
-        m = re.search("'Prob Density', data: \[(.*)\]", page.text)
-        n = re.search("'Cumulative Dist', data: \[(.*)\]", page.text)
-        prob_density = m.group(1)
-        c_dist = n.group(1)
-        x.loc[(i, 'p_dist')] = prob_density
-        x.loc[(i, 'c_dist')] = c_dist
-        x['url'] = url
-        print str(i)
-    return x
-
-
-####### Call to Pull Data from LBNL
-
-df1 = url(df)
-save_to_tsv(df1, outfile='ORIGINAL_LBNL.tsv')
-
-####### Split Data into Cumulative Distributions and Probability Density Distributions
-
-P_Dist = df1.drop('c_dist', axis=1)
-C_Dist = df1.drop('p_dist', axis=1)
-
-####### Pull Coordinates into Unicode Lists of X & Y Values
-
-P_Dist['x_vals'] = P_Dist.apply(lambda x: re.findall('(?<=\[)(.*?)(?=,)', x['p_dist']), axis=1)
-P_Dist['y_vals'] = P_Dist.apply(lambda x: re.findall('(?<=\d,)(.+?)(?=\])', x['p_dist']), axis=1)
-C_Dist['x_vals'] = C_Dist.apply(lambda x: re.findall('(?<=\[)(.*?)(?=,)', x['c_dist']), axis=1)
-C_Dist['y_vals'] = C_Dist.apply(lambda x: re.findall('(?<=\d,)(.+?)(?=\])', x['c_dist']), axis=1)
-
-####### Convert Unicode to Float for X & Y Values
-
-P_Dist['x_vals'] = P_Dist.apply(lambda x: [float(unicode(x['x_vals'][i])) for i in range(len(x['x_vals']))], axis=1)
-P_Dist['y_vals'] = P_Dist.apply(lambda x: [float(unicode(x['y_vals'][i])) for i in range(len(x['y_vals']))], axis=1)
-
-C_Dist['x_vals'] = C_Dist.apply(lambda x: [float(unicode(x['x_vals'][i])) for i in range(len(x['x_vals']))], axis=1)
-C_Dist['y_vals'] = C_Dist.apply(lambda x: [float(unicode(x['y_vals'][i])) for i in range(len(x['y_vals']))], axis=1)
-
-
-####### Put Columns into RECS Format
-
-def process_data(df):
-    field_dicts = {'stories': story_dict,
-                   'Foundation Type': foundation_dict,
-                   'Duct': duct_dict,
-                   'Size': size_dict,
-                   'yearmaderange': vintage_dict,
-                   'Region': region_dict}
-    for field_name, field_dict in field_dicts.iteritems():
-        for num, name in field_dict.iteritems():
-            df.loc[:, field_name].replace(num, name, inplace=True)
-    return df
-
-####### Make Function Calls
-
-P_Dist = process_data(P_Dist)
-C_Dist = process_data(C_Dist)
-
-save_to_tsv(P_Dist, outfile='LBNL_P_Dist.tsv')
-save_to_tsv(C_Dist, outfile='LBNL_C_Dist.tsv')
+#x = {"Size": [1000, 2000, 3000, 4500], "yearmaderange": [1, 2, 3, 4, 5, 6], "WAP": [0, 1], "EE": [0, 1],
+#     "stories": [8, 16], "Region": [1, 2, 3, 4], "Climate_Zone": [1, 2, 3, 4, 5, 6, 7, 8], "Foundation Type": [1, 2, 3],
+#     "Duct": [1, 2, 3]}
+#df = pd.DataFrame(list(itertools.product(*x.values())), columns=x.keys())
+#
+####### Combinations that do not exist on Website
+#
+#lh = {'Region': [1], 'Climate_Zone': [8]}
+#ld = {'Region': [2], 'Climate_Zone': [1, 7]}
+#lm = {'Region': [3], 'Climate_Zone': [1, 2, 5, 6, 7]}
+#la = {'Region': [4], 'Climate_Zone': [1, 2, 3, 4, 5, 6]}
+#lf = [lh, ld, lm, la]
+#
+######## Removal of non-existant combinations
+#
+#for i in range(len(lf)):
+#    LF = lf[i]
+#    df = df.drop(df[df[list(LF)].isin(LF).all(axis=1)].index.tolist())
+#
+#df = df.reset_index()
+#df = df.drop('index', axis=1)
+#df['p_dist'] = ""
+#df['c_dist'] = ""
+#
+#
+######## Create URL and Query
+#
+#def url(x):
+#    for i, row in x.iterrows():
+#        url = 'http://resdb.lbl.gov/main.php?step=2&sub=2&run_env_model=&dtype1=&dtype2=&is_ca=&calc_id=2&floor_area=' + str(
+#            row['Size']) + '&house_height=' + str(row['stories']) + '&year_built=' + str(
+#            row['yearmaderange']) + '&wap=' + str(row['WAP']) + '&ee_home=' + str(row['EE']) + '&region=' + str(
+#            row['Region']) + '&zone=' + str(row['Climate_Zone']) + '&foundation=' + str(
+#            row['Foundation Type']) + '&duct=' + str(row['Duct'])
+#        page = requests.get(url)
+#        m = re.search("'Prob Density', data: \[(.*)\]", page.text)
+#        n = re.search("'Cumulative Dist', data: \[(.*)\]", page.text)
+#        prob_density = m.group(1)
+#        c_dist = n.group(1)
+#        x.loc[(i, 'p_dist')] = prob_density
+#        x.loc[(i, 'c_dist')] = c_dist
+#        x['url'] = url
+#        print str(i)
+#    return x
+#
+#
+######## Call to Pull Data from LBNL
+#
+#df1 = url(df)
+#save_to_tsv(df1, outfile='ORIGINAL_LBNL.tsv')
+#
+######## Split Data into Cumulative Distributions and Probability Density Distributions
+#
+#P_Dist = df1.drop('c_dist', axis=1)
+#C_Dist = df1.drop('p_dist', axis=1)
+#
+######## Pull Coordinates into Unicode Lists of X & Y Values
+#
+#P_Dist['x_vals'] = P_Dist.apply(lambda x: re.findall('(?<=\[)(.*?)(?=,)', x['p_dist']), axis=1)
+#P_Dist['y_vals'] = P_Dist.apply(lambda x: re.findall('(?<=\d,)(.+?)(?=\])', x['p_dist']), axis=1)
+#C_Dist['x_vals'] = C_Dist.apply(lambda x: re.findall('(?<=\[)(.*?)(?=,)', x['c_dist']), axis=1)
+#C_Dist['y_vals'] = C_Dist.apply(lambda x: re.findall('(?<=\d,)(.+?)(?=\])', x['c_dist']), axis=1)
+#
+######## Convert Unicode to Float for X & Y Values
+#
+#P_Dist['x_vals'] = P_Dist.apply(lambda x: [float(unicode(x['x_vals'][i])) for i in range(len(x['x_vals']))], axis=1)
+#P_Dist['y_vals'] = P_Dist.apply(lambda x: [float(unicode(x['y_vals'][i])) for i in range(len(x['y_vals']))], axis=1)
+#
+#C_Dist['x_vals'] = C_Dist.apply(lambda x: [float(unicode(x['x_vals'][i])) for i in range(len(x['x_vals']))], axis=1)
+#C_Dist['y_vals'] = C_Dist.apply(lambda x: [float(unicode(x['y_vals'][i])) for i in range(len(x['y_vals']))], axis=1)
+#
+#
+######## Put Columns into RECS Format
+#
+#def process_data(df):
+#    field_dicts = {'stories': story_dict,
+#                   'Foundation Type': foundation_dict,
+#                   'Duct': duct_dict,
+#                   'Size': size_dict,
+#                   'yearmaderange': vintage_dict,
+#                   'Region': region_dict}
+#    for field_name, field_dict in field_dicts.iteritems():
+#        for num, name in field_dict.iteritems():
+#            df.loc[:, field_name].replace(num, name, inplace=True)
+#    return df
+#
+######## Make Function Calls
+#
+#P_Dist = process_data(P_Dist)
+#C_Dist = process_data(C_Dist)
+#
+#save_to_tsv(P_Dist, outfile='LBNL_P_Dist.tsv')
+#save_to_tsv(C_Dist, outfile='LBNL_C_Dist.tsv')
 
 
 ###### Create Frequency Distribution & Distribution Table
@@ -196,33 +199,65 @@ C_Dist = pd.concat([C_Dist, D_list], axis=1)
 C_Dist = C_Dist.replace(np.NaN, 0)
 C_Dist['Mean'] =  C_Dist.apply(lambda x: mean(x['x_vals'],x['y_vals']), axis =1)
 C_Dist['Var'] = C_Dist.apply(lambda x: var(x['x_vals'],x['y_vals']), axis=1)
-C_Dist = C_Dist.drop(['c_dist','y_vals'],axis=1)
+C_Dist = C_Dist.drop(['c_dist'],axis=1)
 save_to_tsv(C_Dist, outfile='LBNL_FRQ_Dist.tsv')
 
 ##### Bin Different Columns together
-#df1 = increments by 1
-#df2 = increments by 2
-#
-#df = pd.read_csv('LBNL_FRQ_Dist.tsv', sep='\t')
+#df_1 = increments by 1
+#df_2 = increments by 2
 
-##Bin by every 1
-#df1 = pd.DataFrame()
-#x = df.columns.tolist()[12:-2]
-#x5 = x[:19]
-#x1 = x[19:]
-#for i in range(len(x5)//2):
-#    header = (float(x5[2*i]) + float(x5[2*i+1]))/2
-#    print header
-#    df1[header] = df[x5[2*i]]+df[x5[2*i+1]]
-#for i in x1:
-#    df1[i] = df[i]
-#
-##Bin by every 2
-#x = df1.columns.tolist()
-#df2 = pd.DataFrame()
-#for i in range(len(x)//2):
-#    header = (float(x[2*i]) + float(x[2*i+1]))/2
-#    print header
-#    df2[header] = df1[x[2*i]]+df1[x[2*i+1]]
-#
-#df.join(df1)
+df = pd.read_csv('LBNL_FRQ_Dist.tsv', sep='\t')
+
+#Bin by every 1
+
+df_1 = pd.DataFrame()
+x = df.columns.tolist()[14:-2]
+x5 = x[:19]
+x1 = x[19:]
+for i in range(len(x5)//2):
+    header = str((float(x5[2*i]) + float(x5[2*i+1]))/2)
+    print header
+    df_1[header] = df[x5[2*i]]+df[x5[2*i+1]]
+for i in x1:
+    df_1[i] = df[i]
+
+#Create New Dataframe
+
+df_col = df[df.columns[:9]]
+df1 = df_col.join(df_1)
+x_val = df_1.columns.tolist()
+x_val = [float(i) for i in x_val]
+df1['y_vals'] = df_1[df_1.columns[1:]].apply(lambda x: ','.join(x.dropna().astype(float).astype(str)),axis=1)
+df1['y_vals'] = df1.apply(lambda x: literal_eval(x['y_vals']),axis = 1)
+df1['x_vals'] = [x_val] * len(df_1)
+#Bin by every 2
+
+x = df_1.columns.tolist()
+df_2 = pd.DataFrame()
+for i in range(len(x)//2):
+    header = str((float(x[2*i]) + float(x[2*i+1]))/2)
+    print header
+    df_2[header] = df1[x[2*i]]+df1[x[2*i+1]]
+
+df2 = df_col.join(df_2)
+x_val = df_2.columns.tolist()
+x_val = [float(i) for i in x_val]
+df2['y_vals'] = []
+df2['y_vals'] = df_2[df_2.columns[1:]].apply(lambda x: ','.join(x.dropna().astype(float).astype(str)),axis=1)
+df2['y_vals'] = df2.apply(lambda x: literal_eval(x['y_vals']),axis = 1)
+df2['x_vals'] = [x_val] * len(df_2)
+
+save_to_tsv(C_Dist, outfile='LBNL_FRQ_Dist.tsv')
+
+#####Plots of binned vs unbinned
+
+for i in range(3):
+    n = random.randint(0, .5*len(df1))
+    plt.figure()
+    x_val1 = df1.loc[df1.index[n],'x_vals']
+    x_val2 = df1.loc[df2.index[n],'x_vals']
+    y_val1 = df1.loc[df1.index[n],'y_vals']
+    y_val2 = df2.loc[df2.index[n],'y_vals']
+    plt.plot(x_val1,y_val1,':b',label = 'Bin1',linewidth=3)
+    plt.plot(x_val2,y_val2,'--r',label = 'Bin2',linewidth=3)
+    plt.legend();
