5f9f194253316b5fd11a21deea5fcb44b487a2c0
Noel Merket <noel.merket@nrel.gov>
2017-07-07 11:35:01 -0600

    changing to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'BuildStock'
-copyright = u'2017, Noel Merket'
+copyright = u'2017, NREL'
 author = u'Noel Merket'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -89,7 +89,9 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
