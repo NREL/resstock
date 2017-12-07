d1e0f14c7dab554f3debc6fa9d98d36e8cf9058b
Joe Robertson <joseph.robertson@nrel.gov>
2017-12-07 16:30:01 -0700

    Household counts for pums by structure type.

--- data/pums/query_pums_raw_sql.py
+++ data/pums/query_pums_raw_sql.py
@@ -5,7 +5,7 @@ import psycopg2 as pg
 
 con_string = "host={} port={} dbname={} user={} password={}".format(os.environ['GIS_HOST'], os.environ['GIS_PORT'], os.environ['GIS_DBNAME'], os.environ['GIS_USER'], os.environ['GIS_PASSWORD'])
 
-cols = ['serial', 'unitsstr', 'hhincome', 'repwt', 'hhwt', 'builtyr2', 'rooms', 'fuelheat', 'bedrooms', 'region', 'vacancy', 'state_abbr', 'nfams', 'famsize', 'ftotinc', 'numprec', 'gq', 'gqtype']
+cols = ['serial', 'unitsstr', 'hhincome', 'repwt', 'hhwt', 'builtyr2', 'rooms', 'fuelheat', 'bedrooms', 'hhtype', 'vacancy', 'state_abbr', 'nfams', 'numprec']
 
 def retrieve_tables():
     con = pg.connect(con_string)
@@ -110,11 +110,10 @@ if __name__ == '__main__':
     df = retrieve_data(table)
     if df is None:
       continue
-    # df = df.drop_duplicates()
-    # df = df.groupby(['serial', 'unitsstr', 'hhincome', 'repwt', 'hhwt', 'builtyr2', 'rooms', 'fuelheat', 'bedrooms', 'hhtype', 'region', 'stateicp', 'statefip', 'vacancy', 'state_abbr', 'nfams']).sum().reset_index() # for each unique structure, sum / famsize / ftotinc
+    df = df.drop_duplicates()
     df = assign_vintage(df)
     df = assign_heating_fuel(df)
-    df = assign_federal_poverty_level(df)
+    # df = assign_federal_poverty_level(df)
     dfs.append(df)
     
   pd.concat(dfs).to_csv(os.path.join(os.path.dirname(__file__), 'MLR', 'pums.csv'), index=False)
