97abc8f24cadf7e2bdf58e1ed1cda0286ae60a0d
Joe Robertson <joseph.robertson@nrel.gov>
2017-12-07 16:30:01 -0700

    Household counts for pums by structure type.

--- data/pums/input.py
+++ data/pums/input.py
@@ -3,14 +3,23 @@ import sys
 import numpy as np
 import pandas as pd
 
+cols = ['serial', 'unitsstr', 'hhincome', 'repwt', 'hhwt', 'builtyr2', 'rooms', 'fuelheat', 'bedrooms', 'hhtype', 'vacancy', 'state_abbr', 'nfams', 'numprec']
+
 class Create_DFs():
     
   def __init__(self, file):
-    self.session = pd.read_csv(file, index_col=['serial'], usecols=['serial', 'unitsstr', 'hhwt', 'state_abbr'])
-    self.session = self.session[~self.session['unitsstr'].isin([0, 1, 2])] # not NA, mobile home, boat, tent, van, etc.
+    self.session = pd.read_csv(file, index_col=['serial'], usecols=cols)
+    self.session = self.session[self.session['unitsstr'].isin(range(3, 11))] # not NA, mobile home, boat, tent, van, etc.
       
   def pums_sf_vs_mf(self):
     df = self.session
+    
+    df = df[['unitsstr', 'state_abbr', 'hhwt']]    
+    df = df.groupby(['unitsstr', 'state_abbr']).sum()
+    df = df.reset_index()
+    
+    df = df[['unitsstr', 'hhwt']]
+    df = df.groupby(['unitsstr']).sum()
     
     print df
     
