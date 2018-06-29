3ddbdb3ba0073c33075560004487c5aead86b5be
Joe Robertson <joseph.robertson@nrel.gov>
2018-06-29 16:00:03 -0600

    Updating tsv files and options lookup.

---
+++ resources/measures/ResidentialConstructionsFacadesShared/measure.rb
@@ -0,0 +1,82 @@
+# see the URL below for information on how to write OpenStudio measures
+# http://nrel.github.io/OpenStudio-user-documentation/reference/measure_writing_guide/
+
+require "#{File.dirname(__FILE__)}/resources/constants"
+require "#{File.dirname(__FILE__)}/resources/constructions"
+
+# start the measure
+class ProcessConstructionsFacadesShared < OpenStudio::Measure::ModelMeasure
+
+  # human readable name
+  def name
+    return "ResidentialConstructionsFacadesShared"
+  end
+
+  # human readable description
+  def description
+    return "Used to indicate whether specified multifamily building facade(s) are shared with other multifamily building(s)."
+  end
+
+  # human readable description of modeling approach
+  def modeler_description
+    return "Shared walls are assigned adiabatic constructions."
+  end
+
+  # define the arguments that the user will input
+  def arguments(model)
+    args = OpenStudio::Measure::OSArgumentVector.new
+
+    #make a choice argument for model objects
+    building_facades = OpenStudio::StringVector.new
+    building_facades << Constants.FacadeBack
+    building_facades << Constants.FacadeLeft
+    building_facades << Constants.FacadeRight
+    building_facades << "#{Constants.FacadeLeft}, #{Constants.FacadeRight}"
+    building_facades << "#{Constants.FacadeLeft}, #{Constants.FacadeBack}"
+    building_facades << "#{Constants.FacadeBack}, #{Constants.FacadeRight}"
+    building_facades << "#{Constants.FacadeLeft}, #{Constants.FacadeRight}, #{Constants.FacadeBack}"
+
+    #make an argument for shared building facade
+    shared_building_facades = OpenStudio::Measure::OSArgument::makeChoiceArgument("shared_building_facades", building_facades, true)
+    shared_building_facades.setDisplayName("Shared Building Facade(s)")
+    shared_building_facades.setDescription("The facade(s) of the building that are shared. Surfaces on these facades become adiabatic.")
+    shared_building_facades.setDefaultValue("#{Constants.FacadeLeft}, #{Constants.FacadeRight}")
+    args << shared_building_facades
+
+    return args
+  end
+
+  # define what happens when the measure is run
+  def run(model, runner, user_arguments)
+    super(model, runner, user_arguments)
+
+    # use the built-in error checking
+    if !runner.validateUserArguments(arguments(model), user_arguments)
+      return false
+    end
+    
+    shared_building_facades = runner.getStringArgumentValue("shared_building_facades",user_arguments)
+
+    # Get building units
+    units = Geometry.get_building_units(model, runner)
+    if units.nil?
+      return false
+    end
+    
+    if units.length == 1
+      runner.registerWarning("Building is a single unit, and so would not have any shared facades.")
+      return true
+    end
+    
+    if not WallConstructions.apply_adiabatic(runner, model, shared_building_facades)
+      return false
+    end
+
+    return true
+
+  end
+
+end
+
+# register the measure to be used by the application
+ProcessConstructionsFacadesShared.new.registerWithApplication
