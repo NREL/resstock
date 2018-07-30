8e3c95aaf14c375a0e2fe4c66ee98e7fd0c5be36
Joe Robertson <joseph.robertson@nrel.gov>
2018-07-30 15:41:58 -0600

    Adding unit tests for resilience metric report measure.

--- measures/ResilienceMetricsReport/measure.rb
+++ measures/ResilienceMetricsReport/measure.rb
@@ -3,7 +3,7 @@
 # see the URL below for information on how to write OpenStudio measures
 # http://nrel.github.io/OpenStudio-user-documentation/reference/measure_writing_guide/
 
-require 'openstudio'
+require "#{File.dirname(__FILE__)}/resources/unit_conversions"
 
 # start the measure
 class ResilienceMetricsReport < OpenStudio::Measure::ReportingMeasure
@@ -22,60 +22,46 @@ class ResilienceMetricsReport < OpenStudio::Measure::ReportingMeasure
   def arguments
     args = OpenStudio::Measure::OSArgumentVector.new
 
+    #make an argument for optional output variables
+    arg = OpenStudio::Measure::OSArgument::makeStringArgument("output_vars", true)
+    arg.setDisplayName("Output Variables")
+    arg.setDefaultValue("Zone Mean Air Temperature, Zone Air Relative Humidity")
+    args << arg
+
     #make a double argument for minimum comfortable temperature
-    arg = OpenStudio::Measure::OSArgument::makeDoubleArgument("min_temp", true)
-    arg.setDisplayName("Minimum Comfortable Temperature")
-    arg.setUnits("deg F")
-    arg.setDescription("The minimum temperature for which someone is comfortable.")
-    arg.setDefaultValue(60)
+    arg = OpenStudio::Measure::OSArgument::makeStringArgument("min_vals", true)
+    arg.setDisplayName("Minimum Value")
+    arg.setDescription("Lower threshold.")
+    arg.setDefaultValue("60, 5")
     args << arg
 
     #make a double argument for maximum comfortable temperature
-    arg = OpenStudio::Measure::OSArgument::makeDoubleArgument("max_temp", true)
-    arg.setDisplayName("Maximum Comfortable Temperature")
-    arg.setUnits("deg F")
-    arg.setDescription("The maximum temperature for which someone is comfortable.")
-    arg.setDefaultValue(80)
-    args << arg
-
-    #make a double argument for minimum comfortable relative humidity
-    arg = OpenStudio::Measure::OSArgument::makeDoubleArgument("min_hum", true)
-    arg.setDisplayName("Minimum Comfortable Relative Humidity")
-    arg.setUnits("%")
-    arg.setDescription("The minimum relative humidity for which someone is comfortable.")
-    arg.setDefaultValue(10)
-    args << arg
-
-    #make a double argument for maximum comfortable relative humidity
-    arg = OpenStudio::Measure::OSArgument::makeDoubleArgument("max_hum", true)
-    arg.setDisplayName("Maximum Comfortable Relative Humidity")
-    arg.setUnits("%")
-    arg.setDescription("The maximum relative humidity for which someone is comfortable.")
-    arg.setDefaultValue(60)
+    arg = OpenStudio::Measure::OSArgument::makeStringArgument("max_vals", true)
+    arg.setDisplayName("Maximum Value")
+    arg.setDescription("Upper threshold.")
+    arg.setDefaultValue("80, 60")
     args << arg
 
     return args
   end
   
-  def zones
+  def thermal_zones
     return ["Living Zone", "Finished Basement Zone"]
-  end
-
-  def output_vars
-    return ["Zone Mean Air Temperature", "Zone Air Relative Humidity"]
   end
 
   # define the outputs that the measure will create
   def outputs
+    output_vars = ["Zone Mean Air Temperature", "Zone Air Relative Humidity"] # possible list that the user can enter limits for; should get blank column for ones that aren't entered into output_vars arg
     buildstock_outputs = []
     output_vars.each do |output_var|
-      zones.each do |zone|
-        buildstock_outputs << "#{OpenStudio::toUnderscoreCase(zone)}_#{OpenStudio::toUnderscoreCase(output_var)}"
+      thermal_zones.each do |zone|
+        buildstock_outputs << "#{OpenStudio::toUnderscoreCase(zone)}_#{OpenStudio::toUnderscoreCase(output_var)}_below" # hours below lower threshold
+        buildstock_outputs << "#{OpenStudio::toUnderscoreCase(zone)}_#{OpenStudio::toUnderscoreCase(output_var)}_above" # hours above upper threshold
       end
     end
     result = OpenStudio::Measure::OSOutputVector.new
     buildstock_outputs.each do |output|
-        result << OpenStudio::Measure::OSOutput.makeDoubleOutput(output)
+      result << OpenStudio::Measure::OSOutput.makeDoubleOutput(output)
     end
     return result
   end
@@ -92,8 +78,9 @@ class ResilienceMetricsReport < OpenStudio::Measure::ReportingMeasure
       return result
     end
 
+    output_vars = runner.getStringArgumentValue("output_vars",user_arguments).split(",")
     output_vars.each do |output_var|
-      result << OpenStudio::IdfObject.load("Output:Variable,*,#{output_var},Hourly;").get
+      result << OpenStudio::IdfObject.load("Output:Variable,*,#{output_var.strip},Hourly;").get
     end
 
     return result
@@ -109,10 +96,15 @@ class ResilienceMetricsReport < OpenStudio::Measure::ReportingMeasure
     end
 
     # Assign the user inputs to variables
-    min_temp = runner.getDoubleArgumentValue("min_temp", user_arguments)
-    max_temp = runner.getDoubleArgumentValue("max_temp", user_arguments)
-    min_hum = runner.getDoubleArgumentValue("min_hum", user_arguments)
-    max_hum = runner.getDoubleArgumentValue("max_hum", user_arguments)
+    output_vars = runner.getStringArgumentValue("output_vars",user_arguments).split(",")
+    min_vals = runner.getStringArgumentValue("min_vals", user_arguments).split(",")
+    max_vals = runner.getStringArgumentValue("max_vals", user_arguments).split(",")
+
+    # Error checking
+    if output_vars.length != min_vals.length or output_vars.length != max_vals.length
+      runner.registerError("Number of output variable elements specified inconsistent with either number of minimum or maximum values.")
+      return false
+    end
 
     # Get the last model
     model = runner.lastOpenStudioModel
@@ -146,35 +138,26 @@ class ResilienceMetricsReport < OpenStudio::Measure::ReportingMeasure
       return false
     end
 
-    output_vars.each do |output_var|
-      sql.availableKeyValues(ann_env_pd, "Hourly", output_var).each do |key_value|
+    output_vars.each_with_index do |output_var, i|
+      sql.availableKeyValues(ann_env_pd, "Hourly", output_var.strip).each do |key_value|
 
-        next unless zones.any? { |zone| zone.casecmp(key_value) == 0 }
+        next unless thermal_zones.any? { |zone| zone.casecmp(key_value) == 0 }
 
-        resilience_metric = 0
-        timeseries = sql.timeSeries(ann_env_pd, "Hourly", output_var, key_value)
+        timeseries = sql.timeSeries(ann_env_pd, "Hourly", output_var.strip, key_value)
         if timeseries.empty?
-          runner.registerError("No data found for Hourly #{key_value} #{output_var}.")
+
+          runner.registerError("No data found for Hourly #{key_value} #{output_var.strip}.")
           return false
+
         else
+
           timeseries = timeseries.get
-          values = timeseries.values
 
-          timeseries.dateTimes.each_with_index do |date_time, i|
-            case output_var
-            when "Zone Mean Air Temperature"
-              if UnitConversions.convert(values[i], "C", "F") < min_temp or UnitConversions.convert(values[i], "C", "F") > max_temp
-                resilience_metric += 1 # hours
-              end
-            when "Zone Air Relative Humidity"
-              if values[i] < min_hum or values[i] > max_hum
-                resilience_metric += 1 # hours
-              end
-            end
-          end
+          resilience_metric_below, resilience_metric_above = calc_resilience_metric(output_var.strip, timeseries.values, min_vals[i].strip.to_f, max_vals[i].strip.to_f)
+          report_output(runner, "#{key_value} #{output_var.strip} below", resilience_metric_below)
+          report_output(runner, "#{key_value} #{output_var.strip} above", resilience_metric_above)
+
         end
-
-        report_output(runner, "#{key_value} #{output_var}", resilience_metric)
 
       end
     end
@@ -182,6 +165,27 @@ class ResilienceMetricsReport < OpenStudio::Measure::ReportingMeasure
     sql.close()
 
     return true
+  end
+
+  def calc_resilience_metric(output_var, values, min_val, max_val)
+
+    if output_var == "Zone Mean Air Temperature"
+      (0...values.length).to_a.each do |i|
+        values[i] = UnitConversions.convert(values[i], "C", "F")
+      end
+    end
+
+    resilience_metric_below = 0 # hours
+    resilience_metric_above = 0 # hours
+    (0...values.length).to_a.each do |i|
+      if values[i] < min_val
+        resilience_metric_below += 1
+      elsif values[i] > max_val
+        resilience_metric_above += 1
+      end
+    end
+    return resilience_metric_below, resilience_metric_above
+
   end
 
   def report_output(runner, name, val)
