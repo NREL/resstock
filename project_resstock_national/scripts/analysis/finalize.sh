229a071e2998be457e8265f602b105669138b4e7
Joe Robertson <joseph.robertson@nrel.gov>
2018-11-05 11:18:57 -0700

    Server finalize script retrieves results csv and runs successfully.

--- project_resstock_national/scripts/analysis/finalize.sh
+++ project_resstock_national/scripts/analysis/finalize.sh
@@ -8,13 +8,21 @@ cd $SCRIPTPATH
 cd ..
 cd ..
 
-# TODO: install dependencies
+REFUPGS=""
+for REFUPG in "$@"
+do
+REFUPGS="$REFUPGS -u '$REFUPG'"
+done
+
 sudo apt-get update
 sudo apt-get install -y python-pandas python-pyproj
 
 ANALYSISID=$(basename "$PWD")
-SERVERURL="http://ec2-18-212-144-200.compute-1.amazonaws.com" # TODO
-
 RESULTSCSV="results.csv"
-wget -O $RESULTSCSV "$SERVERURL/analyses/$ANALYSISID/download_data.csv?export=true"
-python lib/resources/results_savings_csv.py -r $RESULTSCSV
+
+wget -O $RESULTSCSV "http://web/analyses/$ANALYSISID/download_data.csv?export=true"
+ARGS="-r $RESULTSCSV $REFUPGS -e egrid_subregions -e location -e net_present_value -e reportable_domain -e simple_payback -e source_energy -e total_utility_bill"
+
+CALL="python lib/resources/results_savings_csv.py $ARGS"
+echo "Calling: $CALL"
+eval $CALL
--- scripts/analysis_final.sh
+++ scripts/analysis_final.sh
@@ -8,14 +8,21 @@ cd $SCRIPTPATH
 cd ..
 cd ..
 
+REFUPGS=""
+for REFUPG in "$@"
+do
+REFUPGS="$REFUPGS -u '$REFUPG'"
+done
+
 sudo apt-get update
 sudo apt-get install -y python-pandas python-pyproj
 
 ANALYSISID=$(basename "$PWD")
-SERVERURL="${1##*/}" # TODO: get server url without having to supply it to server finalization argument box
-
 RESULTSCSV="results.csv"
-wget -O $RESULTSCSV "$SERVERURL/analyses/$ANALYSISID/download_data.csv?export=true"
-python lib/resources/results_savings_csv.py -r $RESULTSCSV -e location -e reportable_domain -e egrid_subregions -e source_energy -e total_utility_bill -e simple_payback -e net_present_value
 
-# TODO: how to get results_savings.csv out of web-background? create api endpoint or something?
+wget -O $RESULTSCSV "http://web/analyses/$ANALYSISID/download_data.csv?export=true"
+ARGS="-r $RESULTSCSV $REFUPGS -e egrid_subregions -e location -e net_present_value -e reportable_domain -e simple_payback -e source_energy -e total_utility_bill"
+
+CALL="python lib/resources/results_savings_csv.py $ARGS"
+echo "Calling: $CALL"
+eval $CALL
