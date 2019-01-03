b6db606c5b133187c32c9ef99865898da7a9bc39
Joe Robertson <joseph.robertson@nrel.gov>
2019-01-03 15:21:55 -0700

    Updating to timeseries-updates b0d08da.

--- resources/measures/ResidentialHVACSizing/measure.rb
+++ resources/measures/ResidentialHVACSizing/measure.rb
@@ -7,10 +7,14 @@
 # see the URL below for access to C++ documentation on model objects (click on "model" in the main window to view model objects)
 # http://openstudio.nrel.gov/sites/openstudio.nrel.gov/files/nv_data/cpp_documentation_it/model/html/namespaces.html
 
-require_relative "../HPXMLtoOpenStudio/resources/hvac_sizing"
-require_relative "../HPXMLtoOpenStudio/resources/weather"
-require_relative "../HPXMLtoOpenStudio/resources/constants"
-require_relative "../HPXMLtoOpenStudio/resources/geometry"
+resources_path = File.absolute_path(File.join(File.dirname(__FILE__), "../HPXMLtoOpenStudio/resources"))
+unless File.exists? resources_path
+  resources_path = File.join(OpenStudio::BCLMeasure::userMeasuresDir.to_s, "HPXMLtoOpenStudio/resources") # Hack to run measures in the OS App since applied measures are copied off into a temporary directory
+end
+require File.join(resources_path, "hvac_sizing")
+require File.join(resources_path, "weather")
+require File.join(resources_path, "constants")
+require File.join(resources_path, "geometry")
 
 # start the measure
 class ProcessHVACSizing < OpenStudio::Measure::ModelMeasure
