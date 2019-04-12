76fc5add3170e324b0ea7193bad6033cbbe1ac4e
aspeake1 <andrew.speake@nrel.gov>
2019-04-12 14:38:49 -0600

    edit test file for buildstock directory

--- resources/measures/ResidentialDemandResponse/tests/demand_response_schedule_test.rb
+++ resources/measures/ResidentialDemandResponse/tests/demand_response_schedule_test.rb
@@ -1,4 +1,4 @@
-require_relative '../../../test/minitest_helper'
+require_relative '../../../../test/minitest_helper'
 require 'openstudio'
 require 'openstudio/measure/ShowRunnerOutput'
 require 'minitest/autorun'
