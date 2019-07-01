8cae3dc4c84c7eec4f9186031713a40c83358da3
Joe Robertson <joseph.robertson@nrel.gov>
2019-07-01 12:24:15 -0600

    Few more success to fail places.

--- measures/Outages/tests/Outage_Test.rb
+++ measures/Outages/tests/Outage_Test.rb
@@ -160,7 +160,7 @@ class OutageTest < MiniTest::Test
     result = runner.result
 
     # show the output
-    show_output(result) unless result.value.valueName == 'Success'
+    show_output(result) unless result.value.valueName == 'Fail'
 
     # assert that it didn't run
     assert_equal("Fail", result.value.valueName)
