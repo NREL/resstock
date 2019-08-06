4a401bbe895e238f4767ec6c12c456d8be242ad4
Jon Winkler <jon.winkler@nrel.gov>
2019-08-06 12:29:40 -0600

    Forgot the minus sign.

--- resources/measures/ResidentialHVACAirSourceHeatPumpSingleSpeed/measure.rb
+++ resources/measures/ResidentialHVACAirSourceHeatPumpSingleSpeed/measure.rb
@@ -328,7 +328,7 @@ class ProcessSingleSpeedAirSourceHeatPump < OpenStudio::Measure::ModelMeasure
 
       if rated_cfm_per_ton != actual_cfm_per_ton
         # Use NIST fan power correlation to overwrite fan_power_installed
-        fan_power_installed = 0.00072 * (actual_cfm_per_ton - rated_cfm_per_ton) + fan_power_installed
+        fan_power_installed = -0.00072 * (actual_cfm_per_ton - rated_cfm_per_ton) + fan_power_installed
       end
 
       success = HVAC.apply_central_ashp_1speed(model, unit, runner, seer, hspf, eers, cops, shrs,
