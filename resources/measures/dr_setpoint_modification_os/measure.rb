8a145a188ff40ad30dfbcb6926b8d85923740792
ehale <elaine.hale@nrel.gov>
2019-08-20 15:44:23 -0600

    Revert "Added alternate DR schedule definition measure.  Measure is mutually exclusive with original method that creates a schedule based on a start time and duration.  New method has randomized signal with minimum and maximum holds of 20 and 180 minutes, respectively.  Each of the two schedule creation methods are applied half the time.  Do to randomization, no distribution is applied to the PRBS case."
    This reverts commit 7789de2caf2805f3397ce65258866f07054a5fb3.

--- resources/measures/dr_setpoint_modification_os/measure.rb
+++ resources/measures/dr_setpoint_modification_os/measure.rb
@@ -98,13 +98,8 @@ class DrSetpointModificationOS < OpenStudio::Measure::ModelMeasure
       if dr_event_schedule.is_initialized
         dr_event_schedule = dr_event_schedule.get.to_ScheduleRuleset.get
       else
-				dr_event_schedule = model.getObjectByTypeAndName('OS_Schedule_File'.to_IddObjectType, dr_event_schedule_name)
-				if dr_event_schedule.is_initialized
-					dr_event_schedule = dr_event_schedule.get.to_ScheduleFile.get
-				else
-					runner.registerError("ERROR.  Schedule #{dr_event_schedule_name} cannot be loaded")
-					return false
-				end	
+        runner.registerError("ERROR.  Schedule #{dr_event_schedule_name} cannot be loaded")
+        return false
       end
 
     # copy the heating and cooling setpoint schedules
