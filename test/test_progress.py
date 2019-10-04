e102f7029abfa68e5a1a5150ff198fa434b2f533
Joe Robertson <joseph.robertson@nrel.gov>
2019-10-04 10:30:15 -0600

    Use python for progress example.

---
+++ test/test_progress.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+for filepath in os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../results')):
+  filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../results', filepath)
+  df = pd.read_csv(filepath, index_col=['building_id'])
+  results = {'num_dps': [df.shape[0]], 'total_site_energy_mbtu': [df['simulation_output_report.total_site_energy_mbtu'].sum()]}
+
+  results = pd.DataFrame.from_dict(results)
+  results = results.set_index('num_dps')
+  results.to_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../results/results.csv'))
+  break
