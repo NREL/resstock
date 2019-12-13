25f062d4632eceed154f43bb426d6d061891fa1f
Joe Robertson <joseph.robertson@nrel.gov>
2019-12-13 11:22:36 -0700

    Include 10min, 1min, and leap year schedule files.

--- resources/measures/HPXMLtoOpenStudio/resources/schedules.rb
+++ resources/measures/HPXMLtoOpenStudio/resources/schedules.rb
@@ -794,10 +794,6 @@ class Schedule
 
   # return [Double] The total number of full load hours for this schedule.
   def self.annual_equivalent_full_load_hrs(year_description, schedule)
-    if schedule.to_ScheduleFile.is_initialized
-      # TODO
-    end
-
     if schedule.to_ScheduleInterval.is_initialized
       timeSeries = schedule.to_ScheduleInterval.get.timeSeries
       annual_flh = timeSeries.averageValue * Constants.NumHoursInYear(year_description.isLeapYear)
@@ -1072,25 +1068,46 @@ class SchedulesFile
                          col_name:,
                          rows_to_skip: 1)
 
+    if @schedules[col_name].nil?
+      @runner.registerError("Could not find the '#{col_name}' schedule.")
+      return false
+    end
+
+    col_index = get_col_index(col_name: col_name)
     year_description = @model.getYearDescription
     num_hrs_in_year = Constants.NumHoursInYear(year_description.isLeapYear)
-    @min_per_item = 60.0 / (@schedules[col_name].length / num_hrs_in_year)
-    col_index = get_col_index(col_name: col_name)
+    schedule_length = @schedules[col_name].length
+
+    min_per_item = 60.0 / (schedule_length / num_hrs_in_year)
+    validateMinPerItem(min_per_item: min_per_item)
 
     schedule_file = OpenStudio::Model::ScheduleFile.new(@external_file)
     schedule_file.setName(sch_file_name)
     schedule_file.setColumnNumber(col_index + 1)
     schedule_file.setRowstoSkipatTop(rows_to_skip)
-    schedule_file.setMinutesperItem("#{@min_per_item.to_i}")
+    schedule_file.setMinutesperItem("#{min_per_item.to_i}")
 
     return schedule_file
+  end
+
+  def annual_equivalent_full_load_hrs(col_name:)
+    year_description = @model.getYearDescription
+    num_hrs_in_year = Constants.NumHoursInYear(year_description.isLeapYear)
+    schedule_length = @schedules[col_name].length
+
+    min_per_item = 60.0 / (schedule_length / num_hrs_in_year)
+    validateMinPerItem(min_per_item: min_per_item)
+
+    ann_equiv_full_load_hrs = @schedules[col_name].reduce(:+) / (60.0 / min_per_item)
+
+    return ann_equiv_full_load_hrs
   end
 
   def calcDesignLevelFromAnnualkWh(col_name:,
                                    annual_kwh:)
 
-    equiv_full_load_hrs = @schedules[col_name].reduce(:+) / (60.0 / @min_per_item)
-    design_level = annual_kwh * 1000.0 / equiv_full_load_hrs # W
+    ann_equiv_full_load_hrs = annual_equivalent_full_load_hrs(col_name: col_name)
+    design_level = annual_kwh * 1000.0 / ann_equiv_full_load_hrs # W
 
     return design_level
   end
@@ -1104,19 +1121,28 @@ class SchedulesFile
     return design_level
   end
 
-  def validateValues(values:)
+  def validateValues(col_name:,
+                     values:)
     if values.max > 1
+      @runner.registerError("The max value of schedule '#{col_name}' is greater than 1.")
       @validated = false
     end
-    # TODO: validate min_per_item in [1, 2, 3, ..., 60]
+  end
+
+  def validateMinPerItem(min_per_item:)
+    unless [1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60].include? min_per_item
+      @runner.registerError("Calculated an invalid schedule min_per_item=#{min_per_item}.")
+      @validated = false
+    end
   end
 
   def import
     columns = CSV.read(@schedules_output_path).transpose
     columns.each do |col|
+      col_name = col[0]
       values = col[1..-1].map { |v| v.to_f }
-      validateValues(values: values)
-      @schedules[col[0]] = values
+      validateValues(col_name: col_name, values: values)
+      @schedules[col_name] = values
     end
 
     external_file = OpenStudio::Model::ExternalFile::getExternalFile(@model, @schedules_output_path)
--- resources/measures/ResidentialGeometryCreateSingleFamilyAttached/measure.rb
+++ resources/measures/ResidentialGeometryCreateSingleFamilyAttached/measure.rb
@@ -893,7 +893,7 @@ class CreateResidentialSingleFamilyAttachedGeometry < OpenStudio::Measure::Model
       return false
     end
 
-    sch_path = File.join(File.dirname(__FILE__), "../HPXMLtoOpenStudio/resources/schedules.csv")
+    sch_path = File.join(File.dirname(__FILE__), "../HPXMLtoOpenStudio/resources/schedules_TMY_10min.csv")
     schedule_file = SchedulesFile.new(runner: runner, model: model, schedules_output_path: sch_path)
     if not schedule_file.validated?
       return false
