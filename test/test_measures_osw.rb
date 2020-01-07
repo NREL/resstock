e667b35683c368e7d592600e36809122b56803f3
Joe Robertson <joseph.robertson@nrel.gov>
2020-01-07 15:54:16 -0700

    Cache another test weather file.

--- test/test_measures_osw.rb
+++ test/test_measures_osw.rb
@@ -9,7 +9,7 @@ require_relative '../resources/run_sampling'
 class TestResStockMeasuresOSW < MiniTest::Test
   def test_measures_osw
     project_dir = "project_testing"
-    num_samples = 20
+    num_samples = 1
 
     parent_dir = File.absolute_path(File.join(File.dirname(__FILE__), "test_measures_osw"))
 
