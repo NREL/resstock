d92fe1bebd90a5cdf7f9b64ec1ec41a259b1503b
Joe Robertson <joseph.robertson@nrel.gov>
2020-02-03 10:33:16 -0700

    Move build existing model test into workflows folder.

--- workflows/tests/workflows_test.rb
+++ workflows/tests/workflows_test.rb
@@ -10,7 +10,7 @@ class WorkflowTest < MiniTest::Test
   def test_osw
     all_results = []
     parent_dir = File.absolute_path(File.join(File.dirname(__FILE__), ".."))
-    Dir["#{parent_dir}/*.osw"].each do |osw|
+    Dir["#{parent_dir}/example_*.osw"].each do |osw|
       next if File.basename(osw).include? "out"
 
       add_simulation_output_report(osw)
@@ -18,7 +18,7 @@ class WorkflowTest < MiniTest::Test
       all_results << run_and_check(osw, parent_dir)
     end
 
-    results_dir = File.join(parent_dir, "results")
+    results_dir = File.join(parent_dir, "example_osws_results")
     _rm_path(results_dir)
     write_summary_results(results_dir, all_results)
   end
@@ -52,7 +52,7 @@ class WorkflowTest < MiniTest::Test
     system(command)
     sim_time = (Time.now - simulation_start).round(1)
 
-    # Check all output files exist
+    # Check output file exist
     out_osw = File.join(parent_dir, "out.osw")
     assert(File.exists?(out_osw))
 
