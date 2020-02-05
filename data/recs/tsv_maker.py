cfacd5ee8fa88e2698178659c6fe4a80565e6982
Joe Robertson <joseph.robertson@nrel.gov>
2020-02-05 10:48:18 -0700

    Add misc pool tsv generation to 2015 maker.

--- data/recs/tsv_maker.py
+++ data/recs/tsv_maker.py
@@ -57,7 +57,10 @@ class TSVMaker:
         Returns:
           df (dataframe): A pandas dataframe updated with missing dependency rows, source sample sizes, and source weights.
         """
-        levels = df.index.levels
+        try:
+          levels = df.index.levels # MultiIndex
+        except:
+          levels = [df.index] # Index
         names = df.index.names
         option_cols = df.columns.values
         df = df.reset_index()
@@ -66,6 +69,8 @@ class TSVMaker:
             df = df[0: 0]
 
         for group in itertools.product(*levels):
+            if len(group) == 1 and not 'testing' in project:
+                group = group[0]
             if not group in list(df.groupby(names).groups):
                 data = dict(zip(names, group))
                 data.update(dict(zip(option_cols, [1.0 / len(option_cols)] * len(option_cols))))
