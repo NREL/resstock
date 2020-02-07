68db2404091943759f5da990f57396d00013715e
Joe Robertson <joseph.robertson@nrel.gov>
2020-02-07 08:25:47 -0700

    Apply rubocop.

--- resources/measures/HPXMLtoOpenStudio/resources/appliances.rb
+++ resources/measures/HPXMLtoOpenStudio/resources/appliances.rb
@@ -517,7 +517,7 @@ class ClothesWasher
         cd_space = cd.space.get
         ClothesDryer.remove(runner, cd_space, cd_unit_obj_name, false)
         success, cd_ann_e, cd_ann_f, cd_sch = ClothesDryer.apply(model, unit, runner, cd_cef, cd_mult, cd_space,
-                                                              cd_fuel_type, cd_fuel_split, cw, cd_sch, schedules_file)
+                                                                 cd_fuel_type, cd_fuel_split, cw, cd_sch, schedules_file)
 
         if not success
           return false
