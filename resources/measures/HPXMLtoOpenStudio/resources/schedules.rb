999bf1b5c51c1408527e448f5d92f89c09a7efa9
Rajendra Adhikari <rajendraadhikari.ee@gmail.com>
2020-04-21 12:32:31 -0600

    schedule bug-fix

--- resources/measures/HPXMLtoOpenStudio/resources/schedules.rb
+++ resources/measures/HPXMLtoOpenStudio/resources/schedules.rb
@@ -1129,6 +1129,7 @@ class ScheduleGenerator
             end
             if m >= mins_in_year then break end
           end
+          if m >= mins_in_year then break end
           while @shower_schedule[m / minutes_per_steps] > 0 and m < mins_in_year
             # skip till the end of this slot
             m += 1
