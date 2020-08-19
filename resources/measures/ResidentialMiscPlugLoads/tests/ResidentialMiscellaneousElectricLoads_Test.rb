ee70cbff38293d186280fe9de9c0322c8bc446f7
aspeake1 <andrew.speake@nrel.gov>
2020-08-19 08:36:34 -0600

    saving changes to a conflicted file

--- resources/measures/ResidentialMiscPlugLoads/tests/ResidentialMiscellaneousElectricLoads_Test.rb
+++ resources/measures/ResidentialMiscPlugLoads/tests/ResidentialMiscellaneousElectricLoads_Test.rb
@@ -107,13 +107,8 @@ class ResidentialMiscElectricLoadsTest < MiniTest::Test
     args_hash["mult"] = 1.0
     expected_num_del_objects = {}
     expected_num_new_objects = { "ElectricEquipment" => num_fin_spaces, "ElectricEquipmentDefinition" => num_fin_spaces, "ScheduleFile" => 1 }
-<<<<<<< HEAD
     expected_values = { "Annual_kwh" => 2200 * num_units }
     _test_measure("SFA_4units_1story_FB_UA_3Beds_2Baths_Denver.osm", args_hash, expected_num_del_objects, expected_num_new_objects, expected_values, 0)
-=======
-    expected_values = { "Annual_kwh" => 2003 * num_units }
-    _test_measure("SFA_4units_1story_FB_UA_3Beds_2Baths_Denver.osm", args_hash, expected_num_del_objects, expected_num_new_objects, expected_values, num_units)
->>>>>>> origin/master
   end
 
   def test_multifamily_new_construction
@@ -123,13 +118,8 @@ class ResidentialMiscElectricLoadsTest < MiniTest::Test
     args_hash["mult"] = 1.0
     expected_num_del_objects = {}
     expected_num_new_objects = { "ElectricEquipment" => num_fin_spaces, "ElectricEquipmentDefinition" => num_fin_spaces, "ScheduleFile" => 1 }
-<<<<<<< HEAD
     expected_values = { "Annual_kwh" => 2200 * num_units }
     _test_measure("MF_8units_1story_SL_3Beds_2Baths_Denver.osm", args_hash, expected_num_del_objects, expected_num_new_objects, expected_values, 0)
-=======
-    expected_values = { "Annual_kwh" => 1841 * num_units }
-    _test_measure("MF_8units_1story_SL_3Beds_2Baths_Denver.osm", args_hash, expected_num_del_objects, expected_num_new_objects, expected_values, num_units)
->>>>>>> origin/master
   end
 
   private
