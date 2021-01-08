acdbab852023d7f84218101a70a2a8762db40aa7
Joe Robertson <joseph.robertson@nrel.gov>
2021-01-08 11:37:53 -0700

    Update constants and example osws.

--- measures/ResStockArguments/resources/constants.rb
+++ measures/ResStockArguments/resources/constants.rb
@@ -3,6 +3,13 @@
 class Constants
   def self.excludes
     # don't make these BuildResidentialHPXML arguments into ResStockArguments arguments
-    return ['hpxml_path', 'software_program_used', 'software_program_version', 'setpoint_heating_weekday', 'setpoint_heating_weekend', 'setpoint_cooling_weekday', 'setpoint_cooling_weekend']
+    return ['hpxml_path',
+            'software_program_used',
+            'software_program_version',
+            'setpoint_heating_weekday',
+            'setpoint_heating_weekend',
+            'setpoint_cooling_weekday',
+            'setpoint_cooling_weekend',
+            'geometry_has_flue_or_chimney']
   end
 end
