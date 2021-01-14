e5bc8a6033b4080efe475f91ab7f7bc63639e0ed
Peter Berrill <peter.berrill@yale.edu>
2021-01-14 11:52:44 -0500

    Include IECC 2021 in future insulation slab, and define future Insulation finished roof

--- bs_adjust.R
+++ bs_adjust.R
@@ -9,9 +9,74 @@ load("buildstock100.RData") # just demonstrating with the 100,000 bs.csv
 # rs[rs$State=="TX"| rs$State=="FL" | rs$State=="NY" | rs$State== "NE" | rs$State=="DE" | rs$State == "MD",]$Windows<-"Low-E, Double, Low-Gain"
 
 states<-c("TX","FL","NY","NE","DE","MD") # states which have faster adoption of IECC codes than their ResStock custom regionss
-
+# windows ############ same process for all years 
+# all of the states are predominantly (exception of upper NY state) in regions with low-gain windows specified
 rs[rs$State %in% states,]$Windows<-"Low-E, Double, Low-Gain"
+# ducts do not vary by geography
 
+# Infiltration ############ # same process for all years
+# in FL, bring anything over ACH5 down to ACH5. 
+rs[rs$State=="FL"&rs$Infiltration %in% c("6 ACH50","7 ACH50","8 ACH50","10 ACH50","15 ACH50","20 ACH50","25 ACH50","30 ACH50","40 ACH50","50 ACH50"),]$Infiltration<-"5 ACH50"
+# In all other rogue states, bring anything above ACH3 down to ACH3.
+rs[rs$State %in% c("TX","NY","NE","DE","MD")  &rs$Infiltration %in% c("4 ACH50","5 ACH50", "6 ACH50","7 ACH50","8 ACH50","10 ACH50","15 ACH50","20 ACH50","25 ACH50","30 ACH50","40 ACH50","50 ACH50"),]$Infiltration<-"3 ACH50"
+
+# Insulation crawlspace ########### # no diff b/w IECC 2015 and 2021
+# no rqm for TX or FL (climate zones 3, 2, 1)
+# NY, DE, and MD in CZ 4 go to R10/R13. NE in CZ5 goes to R15/R19
+rs[rs$State %in% c("NY","DE","MD") & rs$Insulation.Crawlspace %in% c("Uninsulated, Vented"),]$Insulation.Crawlspace<-"Ceiling R-13, Vented"
+rs[rs$State %in% c("NY","DE","MD") & rs$Insulation.Crawlspace %in% c("Wall R-5, Unvented") ,]$Insulation.Crawlspace<-"Wall R-10, Unvented"
+rs[rs$State =="NE" & rs$Insulation.Crawlspace %in% c("Uninsulated, Vented","Ceiling R-13, Vented"),]$Insulation.Crawlspace<-"Ceiling R-19, Vented"
+rs[rs$State =="NE" & rs$Insulation.Crawlspace  %in% c("Wall R-5, Unvented","Wall R-10, Unvented") ,]$Insulation.Crawlspace<-"Wall R-15, Unvented" # this is a new option
+
+# Insulation Finished Basement ###########
+# no rqm for TX or FL (climate zones 3, 2, 1)
+# NY, DE, and MD in CZ 4 go to R10. NE in CZ5 goes to R15
+rs[rs$State %in% c("NY","DE","MD") & rs$Insulation.Finished.Basement %in% c("Uninsulated","Wall R-5"),]$Insulation.Finished.Basement<-"Wall R-10"
+rs[rs$State =="NE" & rs$Insulation.Finished.Basement %in% c("Uninsulated","Wall R-5","Wall R-10"),]$Insulation.Finished.Basement<-"Wall R-15"
+
+# Insulation IZ Floor and PB and Fin Roof unchanged
+
+# Insulation Slab ######
+# no rqm for TX or FL (climate zones 3, 2, 1)
+# NY, DE, and MD in CZ 4 go to R10 3ft. NE in CZ5 goes to R10 4ft
+rs[rs$State %in% c("NY","DE","MD") & rs$Insulation.Slab %in% c("2ft R5 Perimeter, R5 Gap"),]$Insulation.Slab<-"2ft R10 Perimeter, R10 Gap"
+rs[rs$State %in% c("NY","DE","MD") & rs$Insulation.Slab %in% c("Uninsulated", "2ft R5 Exterior"),]$Insulation.Slab<-"2ft R10 Exterior"
+
+rs[rs$State %in% c("NE") & rs$Insulation.Slab %in% c("2ft R5 Perimeter, R5 Gap","2ft R10 Perimeter, R10 Gap"),]$Insulation.Slab<-"4ft R10 Perimeter, R10 Gap" # new option
+rs[rs$State %in% c("NE") & rs$Insulation.Slab %in% c("Uninsulated", "2ft R5 Exterior","2ft R10 Exterior"),]$Insulation.Slab<-"4ft R10 Exterior"
+
+# second round, for IECC 2021, applied from 2030s onwards. zone 4 now also goes to 4ft R10. Zone 3 now has a stipulation in IECC 2021, but TX is mostly Zone 2 so leave unchanged
+rs[rs$State %in% c("NY","DE","MD") & rs$Insulation.Slab %in% c("2ft R5 Perimeter, R5 Gap","2ft R10 Perimeter, R10 Gap"),]$Insulation.Slab<-"4ft R10 Perimeter, R10 Gap" # new option
+rs[rs$State %in% c("NY","DE","MD") & rs$Insulation.Slab %in% c("Uninsulated", "2ft R5 Exterior","2ft R10 Exterior"),]$Insulation.Slab<-"4ft R10 Exterior"
+
+# Insulation Unfinished Attic ########
+# TX and FL go to R-38 min. CZ4 and CZ5 go to R-49 min. 
+rs[rs$State %in% c("TX","FL") & rs$Insulation.Unfinished.Attic %in% c("Uninsulated, Vented","Ceiling R-7, Vented","Ceiling R-13, Vented","Ceiling R-19, Vented","Ceiling R-30, Vented"),]$Insulation.Unfinished.Attic<-"Ceiling R-38, Vented"
+rs[rs$State %in% c("NY","DE","MD","NE") & rs$Insulation.Unfinished.Attic %in% c("Uninsulated, Vented","Ceiling R-7, Vented","Ceiling R-13, Vented","Ceiling R-19, Vented","Ceiling R-30, Vented",
+                                                                                "Ceiling R-38, Vented"),]$Insulation.Unfinished.Attic<-"Ceiling R-49, Vented"
+# second round, for IECC 2021, from 2030s. TX FL now go to R-49, and CZ 4 and 5 go to R-60
+rs[rs$State %in% c("TX","FL") & rs$Insulation.Unfinished.Attic %in% c("Ceiling R-38, Vented"),]$Insulation.Unfinished.Attic<-"Ceiling R-49, Vented"
+rs[rs$State %in% c("NY","DE","MD","NE") & rs$Insulation.Unfinished.Attic %in% c("Ceiling R-49, Vented"),]$Insulation.Unfinished.Attic<-"Ceiling R-60, Vented"
+
+# Insulation Unfinished Basement ########### # no diff b/w IECC 2015 and 2021
+# no restriction in TX/FL
+# CZ4 goes to R-13 and CZ5 goes to R-19
+rs[rs$State %in% c("NY","DE","MD") & rs$Insulation.Unfinished.Basement == "Uninsulated",]$Insulation.Unfinished.Basement<-"Ceiling R-13" 
+rs[rs$State %in% c("NE") & rs$Insulation.Unfinished.Basement %in% c("Uninsulated","Ceiling R-13") ,]$Insulation.Unfinished.Basement<-"Ceiling R-19" 
+
+# Insulation Wall ############## # no diff b/w IECC 2015 and 2021
+# In TX, FL, minimum R-15 wood, masonry at least R-7
+# In all other states, minimum R-19 wood. In CZ4, masonry at least R-11, in CZ5, masonry at least R-15
+rs[rs$State %in% c("TX","FL") & rs$Insulation.Wall %in% c("Wood Stud, Uninsulated"),]$Insulation.Wall<-"Wood Stud, R-7"
+rs[rs$State %in% c("NY","DE","MD","NE") & rs$Insulation.Wall %in% c("Wood Stud, Uninsulated","Wood Stud, R-7","Wood Stud, R-11","Wood Stud, R-15"),]$Insulation.Wall<-"Wood Stud, R-19"
+
+rs[rs$State %in% c("TX","FL") & rs$Insulation.Wall %in% c("CMU, 6-in Hollow, Uninsulated","Brick, 12-in, 3-wythe, Uninsulated"),]$Insulation.Wall<-"CMU, 6-in Hollow, R-7"
+rs[rs$State %in% c("NY","DE","MD") & rs$Insulation.Wall %in% c("CMU, 6-in Hollow, Uninsulated","CMU, 6-in Hollow, R-7","Brick, 12-in, 3-wythe, Uninsulated"),]$Insulation.Wall<-"CMU, 6-in Hollow, R-11"
+rs[rs$State %in% c("NE") & rs$Insulation.Wall %in% c("CMU, 6-in Hollow, Uninsulated","CMU, 6-in Hollow, R-7","CMU, 6-in Hollow, R-11","Brick, 12-in, 3-wythe, Uninsulated"),]$Insulation.Wall<-"CMU, 6-in Hollow, R-15"
+
+
+
+# higher adoptions of efficient AC in southern states #################
 SoSt8<-c("DC","DE", "KY","MD","TN","VA","WV")
 SoLR<-c("CR09","CR10","CR11")
 rs$SoCool<-0 # is it a souther cooling state, with regard to efficiency standards?
