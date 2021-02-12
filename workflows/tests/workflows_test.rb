9e1b7f6d475b9ca9736178e55b8f739256bd2045
Joe Robertson <joseph.robertson@nrel.gov>
2021-02-12 10:16:49 -0700

    Refactor example workflow test a bit.

--- workflows/tests/workflows_test.rb
+++ workflows/tests/workflows_test.rb
@@ -7,23 +7,45 @@ require_relative '../../resources/hpxml-measures/HPXMLtoOpenStudio/resources/min
 require_relative '../../resources/buildstock'
 
 class RegressionWorkflowTest < MiniTest::Test
+  def before_setup
+    @top_dir = File.absolute_path(File.join(File.dirname(__FILE__), '..'))
+    @lib_dir = File.join(@top_dir, '..', 'lib')
+    @resources_dir = File.join(@top_dir, '..', 'resources')
+  end
+
+  def after_teardown
+    FileUtils.rm_rf(@lib_dir) if File.exist?(@lib_dir)
+    FileUtils.rm_rf(File.join(@top_dir, 'run'))
+    FileUtils.rm_rf(File.join(@top_dir, 'reports'))
+    FileUtils.rm_rf(File.join(@top_dir, 'generated_files'))
+    FileUtils.rm(File.join(@top_dir, 'out.osw'))
+  end
+
   def test_examples_osw
     all_results = []
-    parent_dir = File.absolute_path(File.join(File.dirname(__FILE__), '..'))
 
     cli_path = OpenStudio.getOpenStudioCLI
-    command = "cd #{parent_dir}/.. && \"#{cli_path}\" tasks.rb update_measures"
-    system(command)
+    command = "cd #{@top_dir}/.. && \"#{cli_path}\" tasks.rb update_measures"
+    # system(command)
 
-    create_lib_folder(parent_dir)
-    Dir["#{parent_dir}/*.osw"].each do |osw|
+    create_lib_folder
+
+    Dir["#{@top_dir}/*.osw"].each do |osw|
       next if File.basename(osw).include? 'out'
 
       puts "\nOSW: #{osw} ...\n"
 
       RunOSWs.add_simulation_output_report(osw)
-      out_osw, result = RunOSWs.run_and_check(osw, parent_dir)
+      out_osw, result = RunOSWs.run_and_check(osw, @top_dir)
       result['OSW'] = File.basename(osw)
+      if osw.include?('single_family_detached')
+        result['build_existing_model.geometry_building_type_recs'] = 'Single-Family Detached'
+      elsif osw.include?('single_family_attached')
+        result['build_existing_model.geometry_building_type_recs'] = 'Single-Family Attached'
+      elsif osw.include?('multifamily')
+        result['build_existing_model.geometry_building_type_recs'] = 'Multi-Family with 5+ Units'
+      end
+      result['build_existing_model.county'] = 'CO, Denver County'
       all_results << result
 
       # Check workflow was successful
@@ -32,17 +54,15 @@ class RegressionWorkflowTest < MiniTest::Test
       assert_equal(data_hash['completed_status'], 'Success')
     end
 
-    results_dir = File.join(parent_dir, 'results')
+    results_dir = File.join(@top_dir, 'results')
     RunOSWs._rm_path(results_dir)
-    RunOSWs.write_summary_results(results_dir, all_results)
+    RunOSWs.write_summary_results(results_dir, all_results, 'feature.csv')
   end
 
   private
 
-  def create_lib_folder(parent_dir)
-    lib_dir = File.join(parent_dir, '../lib') # at top level
-    resources_dir = File.join(parent_dir, '../resources')
-    Dir.mkdir(lib_dir) unless File.exist?(lib_dir)
-    FileUtils.cp_r(resources_dir, lib_dir)
+  def create_lib_folder
+    Dir.mkdir(@lib_dir) unless File.exist?(@lib_dir)
+    FileUtils.cp_r(@resources_dir, @lib_dir)
   end
 end
