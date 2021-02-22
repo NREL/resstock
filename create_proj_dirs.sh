5cb0c16dfb22d9d92c260467895aa7f5b4bc0eb6
Peter Berrill <peter.berrill@yale.edu>
2021-02-22 15:01:36 -0500

    Copy project_national_2020 to directories for each stock scenario

---
+++ create_proj_dirs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# create 2025 scenario directories
+cp -r project_national_2020 project_national_2025_base 
+cp -r project_national_2020 project_national_2025_hiDR 
+cp -r project_national_2020 project_national_2025_hiMF 
+cp -r project_national_2020 project_national_2025_hiDRMF 
+
+# create 2030 scenario directories
+cp -r project_national_2020 project_national_2030_base 
+cp -r project_national_2020 project_national_2030_hiDR 
+cp -r project_national_2020 project_national_2030_hiMF 
+cp -r project_national_2020 project_national_2030_hiDRMF 
+
+# create 2035 scenario directories
+cp -r project_national_2020 project_national_2035_base 
+cp -r project_national_2020 project_national_2035_hiDR 
+cp -r project_national_2020 project_national_2035_hiMF 
+cp -r project_national_2020 project_national_2035_hiDRMF 
+
+# create 2040 scenario directories
+cp -r project_national_2020 project_national_2040_base 
+cp -r project_national_2020 project_national_2040_hiDR 
+cp -r project_national_2020 project_national_2040_hiMF 
+cp -r project_national_2020 project_national_2040_hiDRMF 
+
+# create 2045 scenario directories
+cp -r project_national_2020 project_national_2045_base 
+cp -r project_national_2020 project_national_2045_hiDR 
+cp -r project_national_2020 project_national_2045_hiMF 
+cp -r project_national_2020 project_national_2045_hiDRMF
+
+# create 2050 scenario directories
+cp -r project_national_2020 project_national_2050_base 
+cp -r project_national_2020 project_national_2050_hiDR 
+cp -r project_national_2020 project_national_2050_hiMF 
+cp -r project_national_2020 project_national_2050_hiDRMF  
+
+# create 2055 scenario directories
+cp -r project_national_2020 project_national_2055_base 
+cp -r project_national_2020 project_national_2055_hiDR 
+cp -r project_national_2020 project_national_2055_hiMF 
+cp -r project_national_2020 project_national_2055_hiDRMF 
+
+# create 2060 scenario directories
+cp -r project_national_2020 project_national_2060_base 
+cp -r project_national_2020 project_national_2060_hiDR 
+cp -r project_national_2020 project_national_2060_hiMF 
+cp -r project_national_2020 project_national_2060_hiDRMF 
