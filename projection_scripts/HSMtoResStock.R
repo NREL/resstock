e9dba29c5d80ebe8bb532ae59e4ba76657a2bed8
Peter Berrill <peter.berrill@yale.edu>
2021-02-22 20:21:43 -0500

    Define Type ACS, Vintage, IECC CZ, and County files for all future scenarios

--- projection_scripts/HSMtoResStock.R
+++ projection_scripts/HSMtoResStock.R
@@ -116,8 +116,8 @@ for (l in which(vrs==0)) { # make sure there are no rows with a 0 row sum
 vin<-format(vintage_new,nsmall=6,digits=1,scientific=FALSE)
 ty<-format(type_new,nsmall=6,digits=1,scientific=FALSE)
 # save the 2020 vintage and geometry type acs
-write.table(vin,'../project_national_2020/housing_characteristics/Vintage.tsv',append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
-write.table(ty,'../project_national_2020/housing_characteristics/Geometry Building Type ACS.tsv',append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
+# write.table(vin,'../project_national_2020/housing_characteristics/Vintage.tsv',append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
+# write.table(ty,'../project_national_2020/housing_characteristics/Geometry Building Type ACS.tsv',append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
 tc_pumaold<-tc_puma
 
 ## generate new county and IECC climate zone tsv files
@@ -149,23 +149,24 @@ iecc_new[1,16]<-0
 iecc_new[1,17]<-sum(cz_cty)
 
 # cty_new<-rm_dot(cty_new)
-iecc_new<-rm_dot(iecc_new)
+# iecc_new<-rm_dot(iecc_new)
 cty<-format(as.data.frame(cty_new),nsmall=6,digits=1,scientific=FALSE)
 ie<-format(iecc_new,nsmall=6,digits=0,scientific=FALSE)
 # save the 2020 county and IECC Cz
-# write.table(cty,'../resstock_scenarios/project_national_2020/housing_characteristics/County.tsv',append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
-# write.table(ie,'../resstock_scenarios/project_national_2020/housing_characteristics/ASHRAE IECC Climate Zone 2004.tsv',append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
+# write.table(cty,'../project_national_2020/housing_characteristics/County.tsv',append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
+# write.table(ie,'../project_national_2020/housing_characteristics/ASHRAE IECC Climate Zone 2004.tsv',append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
 
 # now run for scenarios 2025-2060 ############
-load("County_Scenario_SM_Results3.RData") # updated to new outputs on Jan 12, and again to #3 on Feb 2
+load("../../HSM_github/HSM_results/County_Scenario_SM_Results.RData") # path to outputs of stock model scenario runs, see HSM github page
 scenarios<-paste(as.character(rep(seq(2025,2060,5),each=4)), c("base","hiDR","hiMF","hiDRMF"),sep="_")
 Years<-c(2020:2060)
 for (s in 1:32) {
   scen<-scenarios[s]
   year<-as.numeric(substr(scen,1,4))
   stock<-substr(scen,6,nchar(scen))
+  # get columns of geoid, year, total occupied stock by type, occupancy, and vintage ACS (cohort)
   hstock<-as.data.frame(get(paste("smop",stock,sep = "_"))[[3]][[1]][which(Years==year),c(1,2,110:119,130:139,150:159)])
-  for (i in 2:3142){
+  for (i in 2:3142){ # get the same data for all counties
     hstock[i,]<-as.data.frame(get(paste("smop",stock,sep = "_"))[[3]][[i]][which(Years==year),c(1,2,110:119,130:139,150:159)])
   }
   hstockold<-hstock
@@ -177,21 +178,18 @@ for (s in 1:32) {
   }
   hstock[,c(9:12,19:22,29:32)]<-hstock[,c(9:12,19:22,29:32)]-hstockold[,c(9:12,19:22,29:32)] # for scenarios 2030 onwards. the newly construction occupied stock in the new cohorts equals the difference between the total stock in the current year, and the stock 5 years ago. 
   # I then remove negative values to remove decline of new construction cohorts, seeing as we want to represent growth of new construction cohorts here.  Decline is accounted for separately through the weighting factors
-  # With the new approach on modeling stock growth and decline for nc cohorts after their decade officially finishes, I want to represent here only nc cohort construction that happends within the appropriate decade.
+  # With the new approach on modeling stock growth and decline for nc cohorts after their decade officially finishes, I want to represent here only nc cohort construction that happens within the appropriate decade.
   # For instance, in 2035 and 2040 new construction should be new construction in the 2030s cohort only. In 2045 and 2050, new construction in the 2040s cohort only, etc. 
   for (c in c(9:12,19:22,29:32)) { # remove negative values, which come about from stock decline of new cohorts.
       hstock[which(hstock[,c]<0),c]<-0
     }
  
   hstock<-merge(hstock,ctycode)
-  # this should be unccessary now
-  # hstock[hstock$GeoID==22059,]$RS_ID<-"LA, La Salle Parish"
-  # hstock[hstock$GeoID==35013,]$RS_ID<-"NM, Dona Ana County"
-  total<-merge(hstock,puma,by.x = "RS_ID",by.y = "Dependency.County")
+  total<-merge(hstock,puma,by.x = "RS_ID",by.y = "Dependency=County")
   # multiply occupied units by type (3) and cohort (all) by the y matrix to convert from cty to puma to produce a matrix of t-c (30 rows) housing units by puma (2336 cols)
   tc_puma<-t(as.matrix(total[,4:33]))%*%y  # for 2025 onwards
   # tc_puma<-t(as.matrix(total[,77:106]))%*%y # only for the 2020 version
-  rownames(tc_puma)<-rownames(tc_pumaold)
+  rownames(tc_puma)<-rownames(tc_pumaold) # this is needed
   # total$TotOccUnits<-rowSums(total[,c(10:13,20:23,30:33)]) # sum up housing units from new, post-2020 cohorts only. by individual cohort depending on the year
   if (s<9) {total$TotOccUnits<-rowSums(total[,c(10,20,30)])} # 2020s cohort
   if (s>8 & s<17) {total$TotOccUnits<-rowSums(total[,c(11,21,31)])} # 2030s cohort
@@ -199,43 +197,48 @@ for (s in 1:32) {
   if (s>24) {total$TotOccUnits<-rowSums(total[,c(13,23,33)])} # 2030s cohort
   x<-total$TotOccUnits # total new occ housing units by county 
 
-  type_new<-type_acs
+  type_new<-as.data.frame(type_acs)
   type_new[,2:12]<-0
 
-  vintage_new<-vintage
+  vintage_new<-as.data.frame(vintage)
   vintage_new[,3:13]<-0
-  vintage_new$Option.2050s<-vintage_new$Option.2040s<-vintage_new$Option.2030s<-vintage_new$Option.2020s<-0 # add the new cohorts
+  vintage_new$`Option=2050s`<-vintage_new$`Option=2040s`<-vintage_new$`Option=2030s`<-vintage_new$`Option=2020s`<-0 # add the new cohorts
   vintage_new<-vintage_new[,c(1,2,11,3:10,14:17,12,13)]
   tcn<-vintage_new[1:9,] # create template for the type cohort new matrix
   tcn[,3:17]<-0
+  # with new approach, all new units are strictly within cohort.
+  if (s<9) {vintage_new$`Option=2020s`<-1}
+  if (s>8 & s<17) {vintage_new$`Option=2030s`<-1}
+  if (s>16 & s<25) {vintage_new$`Option=2040s`<-1}
+  if (s>24) {vintage_new$`Option=2050s`<-1}
 
 # make this a function to create vintage and type files, arguments are 'all', 'vintage'. 
 for (i in 1:length(puma_list)) { # need to figure this function out, and bring it into the world of new vintages.? For 2020 its fine. For future years will need an equivalent of h20pc and an assumption of the breakout of specific MF types (prob use 2010 split)
 puma_i<-puma_list[i]
-ptc<-vintage[vintage$Dependency.PUMA==puma_i,] # puma matrix of units by type and cohort. For 2020 stock, only 9 vintages
+ptc<-as.data.frame(vintage[vintage$`Dependency=PUMA`==puma_i,]) # puma matrix of units by type and cohort. For 2020 stock, only 9 vintages
 tc<-round(ptc[,3:11]*ptc[,13]) # restrict to numerical data only, col names are vintages
 
 # replace all zero submatrices with equal distribution
 for (a in 1:length(rows)) { # for mf, mh, and sf
-  if (all(tc[get(rows[a]),"Option.2010s"]==0)) {tc[get(rows[a]),"Option.2010s"]<-1/length(unlist(tc[get(rows[a]),"Option.2010s"]))}
+  if (all(tc[get(rows[a]),"Option=2010s"]==0)) {tc[get(rows[a]),"Option=2010s"]<-1/length(unlist(tc[get(rows[a]),"Option=2010s"]))}
 }
 
-tcn$Dependency.PUMA<-puma_i # this seems redundant now, actually maybe not
+tcn$`Dependency=PUMA`<-puma_i # this seems redundant now, actually maybe not
 tcn[,3:17]<-0 # bringing this back too
 # add if statements to make sure only within cohort new construction is considered
-if (s<9) { tcn[mf_row,c("Option.2020s")]<-tc_puma[which(row.names(tc_puma)=="MF_2020"),i]*tc[mf_row,c("Option.2010s")]/sum(tc[mf_row,c("Option.2010s")]);  # use 2010s split of MF types for tc_puma future MF splits
-            tcn[sf_row,c("Option.2020s")]<-tc_puma[which(row.names(tc_puma)=="SF_2020"),i]*tc[sf_row,c("Option.2010s")]/sum(tc[sf_row,c("Option.2010s")]); # use 2010s split of SF types for all future SF splits
-            tcn[mh_row,c("Option.2020s")]<-tc_puma[which(row.names(tc_puma)=="MH_2020"),i]}
-if (s>8 & s<17) {tcn[mf_row,c("Option.2030s")]<-tc_puma[which(row.names(tc_puma)=="MF_2030"),i]*tc[mf_row,c("Option.2010s")]/sum(tc[mf_row,c("Option.2010s")]);
-                  tcn[sf_row,c("Option.2030s")]<-tc_puma[which(row.names(tc_puma)=="SF_2030"),i]*tc[sf_row,c("Option.2010s")]/sum(tc[sf_row,c("Option.2010s")]);
-                  tcn[mh_row,c("Option.2030s")]<-tc_puma[which(row.names(tc_puma)=="MH_2030"),i]}
+if (s<9) { tcn[mf_row,c("Option=2020s")]<-tc_puma[which(row.names(tc_puma)=="MF_2020"),i]*tc[mf_row,c("Option=2010s")]/sum(tc[mf_row,c("Option=2010s")]);  # use 2010s split of MF types for tc_puma future MF splits
+            tcn[sf_row,c("Option=2020s")]<-tc_puma[which(row.names(tc_puma)=="SF_2020"),i]*tc[sf_row,c("Option=2010s")]/sum(tc[sf_row,c("Option=2010s")]); # use 2010s split of SF types for all future SF splits
+            tcn[mh_row,c("Option=2020s")]<-tc_puma[which(row.names(tc_puma)=="MH_2020"),i]}
+if (s>8 & s<17) {tcn[mf_row,c("Option=2030s")]<-tc_puma[which(row.names(tc_puma)=="MF_2030"),i]*tc[mf_row,c("Option=2010s")]/sum(tc[mf_row,c("Option=2010s")]);
+                  tcn[sf_row,c("Option=2030s")]<-tc_puma[which(row.names(tc_puma)=="SF_2030"),i]*tc[sf_row,c("Option=2010s")]/sum(tc[sf_row,c("Option=2010s")]);
+                  tcn[mh_row,c("Option=2030s")]<-tc_puma[which(row.names(tc_puma)=="MH_2030"),i]}
 
-if (s>16 & s<25) { tcn[mf_row,c("Option.2040s")]<-tc_puma[which(row.names(tc_puma)=="MF_2040"),i]*tc[mf_row,c("Option.2010s")]/sum(tc[mf_row,c("Option.2010s")]);
-                    tcn[sf_row,c("Option.2040s")]<-tc_puma[which(row.names(tc_puma)=="SF_2040"),i]*tc[sf_row,c("Option.2010s")]/sum(tc[sf_row,c("Option.2010s")]);
-                    tcn[mh_row,c("Option.2040s")]<-tc_puma[which(row.names(tc_puma)=="MH_2040"),i]}
-if (s>24) {tcn[mf_row,c("Option.2050s")]<-tc_puma[which(row.names(tc_puma)=="MF_2050"),i]*tc[mf_row,c("Option.2010s")]/sum(tc[mf_row,c("Option.2010s")]);
-                    tcn[sf_row,c("Option.2050s")]<-tc_puma[which(row.names(tc_puma)=="SF_2050"),i]*tc[sf_row,c("Option.2010s")]/sum(tc[sf_row,c("Option.2010s")]);
-                    tcn[mh_row,c("Option.2050s")]<-tc_puma[which(row.names(tc_puma)=="MH_2050"),i]}
+if (s>16 & s<25) { tcn[mf_row,c("Option=2040s")]<-tc_puma[which(row.names(tc_puma)=="MF_2040"),i]*tc[mf_row,c("Option=2010s")]/sum(tc[mf_row,c("Option=2010s")]);
+                    tcn[sf_row,c("Option=2040s")]<-tc_puma[which(row.names(tc_puma)=="SF_2040"),i]*tc[sf_row,c("Option=2010s")]/sum(tc[sf_row,c("Option=2010s")]);
+                    tcn[mh_row,c("Option=2040s")]<-tc_puma[which(row.names(tc_puma)=="MH_2040"),i]}
+if (s>24) {tcn[mf_row,c("Option=2050s")]<-tc_puma[which(row.names(tc_puma)=="MF_2050"),i]*tc[mf_row,c("Option=2010s")]/sum(tc[mf_row,c("Option=2010s")]);
+                    tcn[sf_row,c("Option=2050s")]<-tc_puma[which(row.names(tc_puma)=="SF_2050"),i]*tc[sf_row,c("Option=2010s")]/sum(tc[sf_row,c("Option=2010s")]);
+                    tcn[mh_row,c("Option=2050s")]<-tc_puma[which(row.names(tc_puma)=="MH_2050"),i]}
 
 # tcn[sf_row,c("Option.2020s")]<-all[which(row.names(all)=="SF_2020"),i]*tc[sf_row,c("Option.2010s")]/sum(tc[sf_row,c("Option.2010s")]) 
 # tcn[sf_row,c("Option.2030s")]<-all[which(row.names(all)=="SF_2030"),i]*tc[sf_row,c("Option.2010s")]/sum(tc[sf_row,c("Option.2010s")])
@@ -249,20 +252,20 @@ if (s>24) {tcn[mf_row,c("Option.2050s")]<-tc_puma[which(row.names(tc_puma)=="MF_
 
 tcn$sample_weight<-rowSums(tcn[,3:15]) # get sums of housing units by Type ACS
 type_new[i,2:10]<-tcn$sample_weight/sum(tcn$sample_weight) # use counts of housing units by Type ACS to define the type split for this puma for this cohort-year
-vint_prob<-tcn[,3:15]/tcn$sample_weight # calculated probability of vintage for each ACS type
-vint_prob[is.na(vint_prob)]<-0# replaces NAN with 0, this will only happy for types with 0 chance of selection, so should never be a problem
-vintage_new[vintage_new$Dependency.PUMA==puma_i,3:15]<-vint_prob
+# vint_prob<-tcn[,3:15]/tcn$sample_weight # calculated probability of vintage for each ACS type
+# vint_prob[is.na(vint_prob)]<-0# replaces NAN with 0, this will only happy for types with 0 chance of selection, so should never be a problem
+# vintage_new[vintage_new$`Dependency=PUMA`==puma_i,3:15]<-vint_prob
 }
 
-vintage_new<-rm_dot(vintage_new)
-type_new<-rm_dot(type_new)
+# vintage_new<-rm_dot(vintage_new)
+# type_new<-rm_dot(type_new)
 vin<-format(vintage_new,nsmall=6,digits=1,scientific=FALSE)
 ty<-format(type_new,nsmall=6,digits=1,scientific=FALSE)
-fol<-paste('../resstock_scenarios/project_national_',scen,sep="")
-# write.table(vin,paste(fol,'/housing_characteristics/Vintage.tsv',sep=""),append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t') 
-# write.table(ty,paste(fol,'/housing_characteristics/Geometry Building Type ACS.tsv',sep=""),append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t') 
+fol<-paste('../project_national_',scen,sep="")
+write.table(vin,paste(fol,'/housing_characteristics/Vintage.tsv',sep=""),append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
+write.table(ty,paste(fol,'/housing_characteristics/Geometry Building Type ACS.tsv',sep=""),append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
 
-cty_new<-county
+cty_new<-as.data.frame(county)
 cty_new[,2:3109][cty_new[,2:3109]>0]<-1 # turn this into a matching concordance matrix
 cty_new[,3110]<-0 # turn sample count to 0
 cz_cty<-as.matrix(cty_new[,2:3109])%*%diag(x) # creates a 15x3108 matrix of occupied housing units by climate zone and county
@@ -277,17 +280,16 @@ cty_new[,3111]<-rowSums(cz_cty) # this is the sample weight, i.e. total housing 
 # }
 # names(cty_new)[2:3109]<-cty_cn
 
-iecc_new<-iecc
+iecc_new<-as.data.frame(iecc)
 iecc_new[1,1:15]<-rowSums(cz_cty)/sum(cz_cty)
 iecc_new[1,16]<-0 # turn sample count to 0
 iecc_new[1,17]<-sum(cz_cty)
 
 # cty_new<-rm_dot(cty_new)
-iecc_new<-rm_dot(iecc_new)
+# iecc_new<-rm_dot(iecc_new)
 cty<-format(as.data.frame(cty_new),nsmall=6,digits=1,scientific=FALSE)
 ie<-format(iecc_new,nsmall=6,digits=0,scientific=FALSE)
 
 write.table(cty,paste(fol,'/housing_characteristics/County.tsv',sep=""),append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
-# write.table(ie,paste(fol,'/housing_characteristics/ASHRAE IECC Climate Zone 2004.tsv',sep=""),append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
-
+write.table(ie,paste(fol,'/housing_characteristics/ASHRAE IECC Climate Zone 2004.tsv',sep=""),append = FALSE,quote = FALSE, row.names = FALSE, col.names = TRUE,sep='\t')
 }
