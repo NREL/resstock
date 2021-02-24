13a0f30e2fa954a3a955c2760db0a066711f9828
Joe Robertson <joseph.robertson@nrel.gov>
2021-02-23 20:47:45 -0700

    Refactor parts of apply upgrade measure.

--- measures/ApplyUpgrade/resources/constants.rb
+++ measures/ApplyUpgrade/resources/constants.rb
@@ -32,4 +32,20 @@ class Constants
       'Slab Perimeter, Exposed, Conditioned (ft)',
     ]
   end
+
+  def self.heating_system_id
+    return 'HeatingSystem'
+  end
+
+  def self.second_heating_system_id
+    return 'SecondHeatingSystem'
+  end
+
+  def self.cooling_system_id
+    return 'CoolingSystem'
+  end
+
+  def self.heat_pump_id
+    return 'HeatPump'
+  end
 end
