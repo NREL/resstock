8af06ef54407ffca8f7c493a217347be9ddf2980
jmaguire1 <Jeff.Maguire@nrel.gov>
2021-05-24 15:49:09 -0600

    Updates to the natural ventilation during an outage

--- resources/measures/HPXMLtoOpenStudio/resources/airflow.rb
+++ resources/measures/HPXMLtoOpenStudio/resources/airflow.rb
@@ -1383,15 +1383,15 @@ class Airflow
 
     # Add a new sensor here for the outage schedule (otg_availability_schedule)
     otg_sensor = OpenStudio::Model::EnergyManagementSystemSensor.new(model, "Schedule Value")
-    otg_sensor.setName("#{obj_name_natvent.name} outage s")
-    otg_sensor.setKeyName(otg_sensor_name.to_s)
+    otg_sensor.setName("#{obj_name_natvent} outage s")
+    otg_sensor.setKeyName(model.alwaysOnDiscreteSchedule.name.to_s)
 
     nvavail_sensor = OpenStudio::Model::EnergyManagementSystemSensor.new(model, "Schedule Value")
-    nvavail_sensor.setName("#{obj_name_natvent.name} nva s")
+    nvavail_sensor.setName("#{obj_name_natvent} nva s")
     nvavail_sensor.setKeyName(avail_sch.name.to_s)
 
     nvsp_sensor = OpenStudio::Model::EnergyManagementSystemSensor.new(model, "Schedule Value")
-    nvsp_sensor.setName("#{obj_name_natvent.name} sp s")
+    nvsp_sensor.setName("#{obj_name_natvent} sp s")
     nvsp_sensor.setKeyName(nv_output.temp_sch.schedule.name.to_s)
 
     # Actuator
@@ -1420,7 +1420,10 @@ class Airflow
     nv_program.addLine("Set MRH = #{nat_vent.max_oa_rh}")
     nv_program.addLine("Set temp1 = (#{nvavail_sensor.name}*NVA)")
     nv_program.addLine("Set SGNV = temp1*((((Cs*dT)+(Cw*(#{vwind_sensor.name}^2)))^0.5)/1000)")
-    nv_program.addLine("If (#{wout_sensor.name}<MHR) && (pt<MRH) && (#{tin_sensor.name}>#{nvsp_sensor.name}) || (#{otg_sensor.name} > 0 && #{tin_sensor.name} > #{tout_sensor.name}")
+    nv_program.addLine("If (#{otg_sensor.name} > 0) && (#{tin_sensor.name} > #{tout_sensor.name}) && (#{tin_sensor.name} > #{nvsp_sensor.name})")
+    nv_program.addLine("  Set SGNV = NVA * ((((Cs*dT)+(Cw*(#{vwind_sensor.name}^2)))^0.5)/1000)") #Recalculate airflow regardless of availability sensor if it's an outage
+    nv_program.addLine("  Set #{natvent_flow_actuator.name} = @Min SGNV MNV")
+    nv_program.addLine("ElseIf (#{wout_sensor.name}<MHR) && (pt<MRH) && (#{tin_sensor.name}>#{nvsp_sensor.name})")
     nv_program.addLine("  Set temp2 = (#{tin_sensor.name}-#{nvsp_sensor.name})")
     nv_program.addLine("  Set NVadj1 = temp2/(#{tin_sensor.name}-#{tout_sensor.name})")
     nv_program.addLine("  Set NVadj2 = (@Min NVadj1 1)")
