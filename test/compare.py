c3ef376960cd9d59f49fb12ee8fb0ee9bf1be43e
Joe Robertson <joseph.robertson@nrel.gov>
2021-06-10 07:55:18 -0700

    Better rounding and sorting in sampling compare script.

--- test/compare.py
+++ test/compare.py
@@ -17,9 +17,12 @@ def value_counts(df, file):
         continue
 
       value_count = df[col].value_counts(normalize=True)
+      value_count = value_count.round(2)
+      keys_to_values = dict(zip(value_count.index.values, value_count.values))
+      keys_to_values = dict(sorted(keys_to_values.items(), key=lambda x: (x[1], x[0]), reverse=True))
       value_counts.append([value_count.name])
-      value_counts.append(value_count.index.values)
-      value_counts.append(value_count.values)
+      value_counts.append(keys_to_values.keys())
+      value_counts.append(keys_to_values.values())
       value_counts.append('\n')
 
     w = csv.writer(f)
