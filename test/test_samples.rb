15029525f566f26e6cebf5a67161bd25d4838bac
Joe Robertson <joseph.robertson@nrel.gov>
2021-06-17 10:40:08 -0700

    Lead with zeroes to clean up sorting.

--- test/test_samples.rb
+++ test/test_samples.rb
@@ -59,8 +59,8 @@ class TestResStockMeasuresOSW < MiniTest::Test
           RunOSWs.add_simulation_output_report(osw)
 
           out_osw, result_characteristics, result_output = RunOSWs.run_and_check(osw, File.join(parent_dir, worker_folder))
-          result_characteristics['OSW'] = "#{project_dir}-#{building_id}.osw"
-          result_output['OSW'] = "#{project_dir}-#{building_id}.osw"
+          result_characteristics['OSW'] = "#{project_dir}-#{building_id.to_s.rjust(3, '0')}.osw"
+          result_output['OSW'] = "#{project_dir}-#{building_id.to_s.rjust(3, '0')}.osw"
           all_results_characteristics << result_characteristics
           all_results_output << result_output
 
