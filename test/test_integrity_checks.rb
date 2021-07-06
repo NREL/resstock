099c0670081b07ba9b45212192782641e5f98505
Scott Horowitz <scott.horowitz@nrel.gov>
2021-07-06 16:44:18 -0600

    Speeds up the code that checks for duplicate assigned arguments.

--- test/test_integrity_checks.rb
+++ test/test_integrity_checks.rb
@@ -199,7 +199,8 @@ class TestResStockErrors < MiniTest::Test
       integrity_check(@project_dir_name, housing_characteristics_dir, lookup_file)
       integrity_check_options_lookup_tsv(@project_dir_name, housing_characteristics_dir, lookup_file)
     rescue Exception => e
-      assert(e.message.include? 'ERROR: Duplicate measure argument assignment(s) across ["Cooling Setpoint", "Cooling Setpoint Offset Magnitude"] parameters. (ResidentialHVACCoolingSetpoints => ["weekday_offset_magnitude", "weekend_offset_magnitude"]) already assigned.')
+      assert(e.message.include? 'ERROR: Duplicate measure argument assignment(s) across ["Cooling Setpoint", "Cooling Setpoint Offset Magnitude"] parameters. "weekday_offset_magnitude" assigned twice.')
+      assert(e.message.include? 'ERROR: Duplicate measure argument assignment(s) across ["Cooling Setpoint", "Cooling Setpoint Offset Magnitude"] parameters. "weekend_offset_magnitude" assigned twice.')
     else
       flunk "Should have caused an error but didn't."
     end
