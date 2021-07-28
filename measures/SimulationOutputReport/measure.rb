1ffcbd08907f317f20ca3453e8ad61359dad7d94
Joe Robertson <joseph.robertson@nrel.gov>
2021-07-28 13:48:37 -0700

    Use new past runner method to get upgrade costs.

--- measures/SimulationOutputReport/measure.rb
+++ measures/SimulationOutputReport/measure.rb
@@ -396,6 +396,7 @@ class SimulationOutputReport < OpenStudio::Measure::ReportingMeasure
     upgrade_cost_name = 'upgrade_cost_usd'
 
     # Get upgrade cost value/multiplier pairs and lifetimes from the upgrade measure
+    values = get_values_from_runner_past_results(runner, 'apply_upgrade')
     has_costs = false
     option_cost_pairs = {}
     option_lifetimes = {}
@@ -403,16 +404,16 @@ class SimulationOutputReport < OpenStudio::Measure::ReportingMeasure
       option_cost_pairs[option_num] = []
       option_lifetimes[option_num] = nil
       for cost_num in 1..num_costs_per_option # Sync with ApplyUpgrade measure
-        cost_value = get_value_from_runner_past_results(runner, "option_%02d_cost_#{cost_num}_value_to_apply" % option_num, 'apply_upgrade', false)
+        cost_value = values["option_%02d_cost_#{cost_num}_value_to_apply" % option_num]
         next if cost_value.nil?
 
-        cost_mult_type = get_value_from_runner_past_results(runner, "option_%02d_cost_#{cost_num}_multiplier_to_apply" % option_num, 'apply_upgrade', false)
+        cost_mult_type = values["option_%02d_cost_#{cost_num}_multiplier_to_apply" % option_num]
         next if cost_mult_type.nil?
 
         has_costs = true
         option_cost_pairs[option_num] << [cost_value.to_f, cost_mult_type]
       end
-      lifetime = get_value_from_runner_past_results(runner, 'option_%02d_lifetime_to_apply' % option_num, 'apply_upgrade', false)
+      lifetime = values['option_%02d_lifetime_to_apply' % option_num]
       next if lifetime.nil?
 
       option_lifetimes[option_num] = lifetime.to_f
