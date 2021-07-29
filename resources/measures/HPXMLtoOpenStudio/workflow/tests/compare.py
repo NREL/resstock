47a0f50f82fee3c3bcab6af0f9a8bacdd5187c00
Joe Robertson <joseph.robertson@nrel.gov>
2021-07-29 08:09:24 -0700

    Comparing strings requires identical columns.

--- resources/measures/HPXMLtoOpenStudio/workflow/tests/compare.py
+++ resources/measures/HPXMLtoOpenStudio/workflow/tests/compare.py
@@ -17,6 +17,14 @@ class BaseCompare:
     def intersect_rows(df1, df2):
           return df1[df1.index.isin(df2.index)]
 
+    @staticmethod
+    def union_columns(df1, df2):
+          cols = sorted(list(set(df1.columns) | set(df2.columns)))
+          for col in cols:
+              if not col in df1.columns:
+                  df1[col] = np.nan
+          return df1[cols]
+
     def results(self, aggregate_column=None, aggregate_function=None, excludes=[], enum_maps={}):
         aggregate_columns = []
         if aggregate_column:
@@ -41,6 +49,8 @@ class BaseCompare:
             try:
                 df = feature_df - base_df
             except BaseException:
+                base_df = self.union_columns(base_df, feature_df)
+                feature_df = self.union_columns(feature_df, base_df)
                 df = feature_df != base_df
                 df = df.astype(int)
 
