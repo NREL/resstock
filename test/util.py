f4c7628037e98ac6c538112f5fe25e98120ccb70
Joe Robertson <joseph.robertson@nrel.gov>
2021-09-15 07:25:25 -0700

    Move util py and add qoi reports.

--- test/util.py
+++ test/util.py
@@ -19,29 +19,29 @@ df = df.rename(columns={'building_id': 'OSW'})
 del df['job_id']
 
 build_existing_models = []
-apply_upgrades = []
 simulation_output_reports = ['color_index']
+qoi_reports = []
 
 for col in df.columns.values:
   if 'applicable' in col:
     continue
-  if col.startswith('apply_upgrade'):
-    apply_upgrades.append(col)
+
   elif col.startswith('build_existing_model'):
     build_existing_models.append(col)
   elif col.startswith('simulation_output_report'):
     simulation_output_reports.append(col)
-
-for col in apply_upgrades:
-  del df[col]
+  elif col.startswith('qoi_report'):
+    qoi_reports.append(col)
 
 results_characteristics = df[['OSW'] + build_existing_models]
 for col in results_characteristics.columns.values:
   results_characteristics = results_characteristics.rename(columns={col: col.replace('build_existing_model.', '')})
 
-results_output = df[['OSW'] + simulation_output_reports]
+results_output = df[['OSW'] + simulation_output_reports + qoi_reports]
+results_output = results_output.round(1)
 for col in results_output.columns.values:
   results_output = results_output.rename(columns={col: col.replace('simulation_output_report.', '')})
+  results_output = results_output.rename(columns={col: col.replace('qoi_report.', '')})
 
 results_characteristics.set_index('OSW').to_csv('baseline/results/results_characteristics.csv')
 results_output.set_index('OSW').to_csv('baseline/results/results_output.csv')
