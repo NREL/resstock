166cf700479809a380447dffa1fe5d10adfc8289
Joe Robertson <joseph.robertson@nrel.gov>
2021-11-04 16:34:45 -0700

    Remove test xmls and refactor apply upgrade tests.

--- measures/ApplyUpgrade/tests/apply_upgrade_test.rb
+++ measures/ApplyUpgrade/tests/apply_upgrade_test.rb
@@ -4,11 +4,12 @@ require 'openstudio'
 
 require_relative '../measure.rb'
 require_relative '../../../resources/hpxml-measures/HPXMLtoOpenStudio/resources/minitest_helper'
-require_relative '../../../resources/hpxml-measures/HPXMLtoOpenStudio/measure'
 
 class ApplyUpgradeTest < MiniTest::Test
   def test_SFD_1story_FB_UA_GRG_MSHP_FuelTanklessWH
-    # Retain the heat pump capacities since you aren't upgrading the heat pump
+    osw_file = '../../UpgradeCosts/tests/SFD_1story_FB_UA_GRG_MSHP_FuelTanklessWH.osw'
+    puts "\nTesting #{File.basename(osw_file)}..."
+
     args_hash = {}
     expected_capacities = {
       'heating_system_heating_capacity' => nil,
@@ -16,26 +17,29 @@ class ApplyUpgradeTest < MiniTest::Test
       'cooling_system_cooling_capacity' => nil,
       'heat_pump_heating_capacity' => 60000.0,
       'heat_pump_cooling_capacity' => 60000.0,
-      'heat_pump_backup_heating_capacity' => 100000.0,
+      'heat_pump_backup_heating_capacity' => 100000.0
     }
-    _test_retaining_capacities('SFD_1story_FB_UA_GRG_MSHP_FuelTanklessWH.xml', args_hash, expected_capacities)
 
-    # Don't retain the heat pump capacities because you are upgrading the heat pump
-    args_hash['heat_pump_type'] = 'air-to-air'
-    expected_capacities['heat_pump_heating_capacity'] = nil
-    expected_capacities['heat_pump_cooling_capacity'] = nil
-    expected_capacities['heat_pump_backup_heating_capacity'] = nil
-    _test_retaining_capacities('SFD_1story_FB_UA_GRG_MSHP_FuelTanklessWH.xml', args_hash, expected_capacities)
+    _lighting_upgrade(args_hash, expected_capacities)
+    _test_retaining_capacities(osw_file, args_hash, expected_capacities)
 
-    # Don't retain the heat pump capacities, but inputs are ignored anyway
-    args_hash = {}
-    args_hash['heating_system_type'] = 'Furnace'
-    args_hash['heat_pump_type'] = 'none'
-    _test_retaining_capacities('SFD_1story_FB_UA_GRG_MSHP_FuelTanklessWH.xml', args_hash, expected_capacities)
+    _heating_system_upgrade(args_hash, expected_capacities)
+    _test_retaining_capacities(osw_file, args_hash, expected_capacities)
+
+    _heating_system_2_upgrade(args_hash, expected_capacities)
+    _test_retaining_capacities(osw_file, args_hash, expected_capacities)
+
+    _cooling_system_upgrade(args_hash, expected_capacities)
+    _test_retaining_capacities(osw_file, args_hash, expected_capacities)
+
+    _heat_pump_upgrade(args_hash, expected_capacities)
+    _test_retaining_capacities(osw_file, args_hash, expected_capacities)
   end
 
   def test_SFD_1story_UB_UA_GRG_ACV_FuelFurnace_PortableHeater_HPWH
-    # Retain the heating system, second heating system, cooling system capacities since you aren't upgrading them
+    osw_file = '../../UpgradeCosts/tests/SFD_1story_UB_UA_GRG_ACV_FuelFurnace_PortableHeater_HPWH.osw'
+    puts "\nTesting #{File.basename(osw_file)}..."
+
     args_hash = {}
     expected_capacities = {
       'heating_system_heating_capacity' => 100000.0,
@@ -43,48 +47,110 @@ class ApplyUpgradeTest < MiniTest::Test
       'cooling_system_cooling_capacity' => 60000.0,
       'heat_pump_heating_capacity' => nil,
       'heat_pump_cooling_capacity' => nil,
-      'heat_pump_backup_heating_capacity' => nil,
+      'heat_pump_backup_heating_capacity' => nil
     }
-    _test_retaining_capacities('SFD_1story_UB_UA_GRG_ACV_FuelFurnace_PortableHeater_HPWH.xml', args_hash, expected_capacities)
 
-    # Don't retain the heating system capacity because you are upgrading the heating system
-    args_hash['heating_system_type'] = 'Furnace'
-    expected_capacities['heating_system_heating_capacity'] = nil
-    _test_retaining_capacities('SFD_1story_UB_UA_GRG_ACV_FuelFurnace_PortableHeater_HPWH.xml', args_hash, expected_capacities)
+    _lighting_upgrade(args_hash, expected_capacities)
+    _test_retaining_capacities(osw_file, args_hash, expected_capacities)
 
-    # Don't retain the second heating system capacity because you are upgrading the second heating system
-    args_hash['heating_system_2_type'] = 'Fireplace'
-    expected_capacities['heating_system_2_heating_capacity'] = nil
-    _test_retaining_capacities('SFD_1story_UB_UA_GRG_ACV_FuelFurnace_PortableHeater_HPWH.xml', args_hash, expected_capacities)
+    _heating_system_upgrade(args_hash, expected_capacities)
+    _test_retaining_capacities(osw_file, args_hash, expected_capacities)
 
-    # Don't retain the cooling system capacity because you are upgrading the cooling system
-    args_hash['cooling_system_type'] = 'room air conditioner'
-    expected_capacities['cooling_system_cooling_capacity'] = nil
-    _test_retaining_capacities('SFD_1story_UB_UA_GRG_ACV_FuelFurnace_PortableHeater_HPWH.xml', args_hash, expected_capacities)
+    _heating_system_2_upgrade(args_hash, expected_capacities)
+    _test_retaining_capacities(osw_file, args_hash, expected_capacities)
 
-    # Don't retain the heating system, second heating system, cooling system capacities, but inputs are ignored anyway
-    args_hash['heat_pump_type'] = 'mini-split'
-    args_hash['heating_system_type'] = 'none'
-    args_hash['heating_system_2_type'] = 'none'
-    args_hash['cooling_system_type'] = 'none'
-    _test_retaining_capacities('SFD_1story_UB_UA_GRG_ACV_FuelFurnace_PortableHeater_HPWH.xml', args_hash, expected_capacities)
+    _cooling_system_upgrade(args_hash, expected_capacities)
+    _test_retaining_capacities(osw_file, args_hash, expected_capacities)
+
+    _heat_pump_upgrade(args_hash, expected_capacities)
+    _test_retaining_capacities(osw_file, args_hash, expected_capacities)
   end
 
   private
 
-  def _test_retaining_capacities(hpxml_file, args_hash, expected_capacities)
+  def _lighting_upgrade(args_hash, expected_capacities)
+    puts "\twindow upgrade..."
+    args_hash['window_ufactor'] = 0.29
+    args_hash['window_shgc'] = 0.26
+  end
+
+  def _heating_system_upgrade(args_hash, expected_capacities)
+    puts "\theating system upgrade..."
+    args_hash['heating_system_type'] = HPXML::HVACTypeFurnace
+    args_hash['heat_pump_type'] = 'none'
+    expected_capacities['heating_system_heating_capacity'] = nil
+    expected_capacities['heat_pump_heating_capacity'] = nil
+    expected_capacities['heat_pump_cooling_capacity'] = nil
+    expected_capacities['heat_pump_backup_heating_capacity'] = nil
+  end
+
+  def _heating_system_2_upgrade(args_hash, expected_capacities)
+    puts "\tsecondary heating system upgrade..."
+    args_hash['heating_system_2_type'] = HPXML::HVACTypeFireplace
+    expected_capacities['heating_system_2_heating_capacity'] = nil
+  end
+
+  def _cooling_system_upgrade(args_hash, expected_capacities)
+    puts "\tcooling system upgrade..."
+    args_hash['cooling_system_type'] = HPXML::HVACTypeCentralAirConditioner
+    args_hash['heat_pump_type'] = 'none'
+    expected_capacities['cooling_system_cooling_capacity'] = nil
+    expected_capacities['heat_pump_heating_capacity'] = nil
+    expected_capacities['heat_pump_cooling_capacity'] = nil
+    expected_capacities['heat_pump_backup_heating_capacity'] = nil
+  end
+
+  def _heat_pump_upgrade(args_hash, expected_capacities)
+    puts "\theat pump upgrade..."
+    args_hash['heating_system_type'] = 'none'
+    args_hash['cooling_system_type'] = 'none'
+    args_hash['heat_pump_type'] = HPXML::HVACTypeHeatPumpAirToAir
+    expected_capacities['heating_system_heating_capacity'] = nil
+    expected_capacities['cooling_system_cooling_capacity'] = nil
+    expected_capacities['heat_pump_heating_capacity'] = nil
+    expected_capacities['heat_pump_cooling_capacity'] = nil
+    expected_capacities['heat_pump_backup_heating_capacity'] = nil
+  end
+
+  def _test_retaining_capacities(osw_file, args_hash, expected_capacities)
+    require 'json'
+
     this_dir = File.dirname(__FILE__)
-    hpxml_path = File.absolute_path("#{this_dir}/#{hpxml_file}")
+    osw = File.absolute_path("#{this_dir}/#{osw_file}")
 
-    puts "\nTesting #{File.basename(hpxml_path)} with #{args_hash}..."
+    measures = {}
 
-    hpxml = HPXML.new(hpxml_path: hpxml_path)
+    osw_hash = JSON.parse(File.read(osw))
+    measures_dir = File.join(File.dirname(__FILE__), osw_hash['measure_paths'][0])
+    osw_hash['steps'].each do |step|
+      measures[step['measure_dir_name']] = [step['arguments']]
+    end
 
-    # create an instance of the measure
+    model = OpenStudio::Model::Model.new
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+
+    # Apply measure
+    success = apply_measures(measures_dir, measures, runner, model)
+
+    # Report warnings/errors
+    runner.result.stepWarnings.each do |s|
+      puts "Warning: #{s}"
+    end
+    runner.result.stepErrors.each do |s|
+      puts "Error: #{s}"
+    end
+
+    assert(success)
+
+    hpxml_path = File.join(this_dir, '../../UpgradeCosts/tests/in.xml')
+    hpxml_in = HPXML.new(hpxml_path: hpxml_path)
+
+    # Create instance of the measure
     measure = ApplyUpgrade.new
 
-    system_upgrades = measure.get_system_upgrades(hpxml, [], args_hash)
-    actual_capacities = measure.get_system_capacities(hpxml, system_upgrades)
+    # Check for correct capacity values
+    system_upgrades = measure.get_system_upgrades(hpxml_in, [], args_hash)
+    actual_capacities = measure.get_system_capacities(hpxml_in, system_upgrades)
 
     expected_capacities.each do |str, val|
       if val.nil?
