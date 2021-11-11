0648bed4e28e867197374c104f5db55d5897493b
aspeake1 <andrew.speake@nrel.gov>
2021-11-11 08:51:04 -0700

    first pass at new measure calling hescore2hpmxl translator

---
+++ measures/HEScoreHPXML/measure.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+# see the URL below for information on how to write OpenStudio measures
+# http://nrel.github.io/OpenStudio-user-documentation/measures/measure_writing_guide/
+
+require 'openstudio'
+if File.exist? File.absolute_path(File.join(File.dirname(__FILE__), '../../lib/resources/hpxml-measures/HPXMLtoOpenStudio/resources')) # Hack to run ResStock on AWS
+  resources_path = File.absolute_path(File.join(File.dirname(__FILE__), '../../lib/resources/hpxml-measures/HPXMLtoOpenStudio/resources'))
+elsif File.exist? File.absolute_path(File.join(File.dirname(__FILE__), '../../resources/hpxml-measures/HPXMLtoOpenStudio/resources')) # Hack to run ResStock unit tests locally
+  resources_path = File.absolute_path(File.join(File.dirname(__FILE__), '../../resources/hpxml-measures/HPXMLtoOpenStudio/resources'))
+elsif File.exist? File.join(OpenStudio::BCLMeasure::userMeasuresDir.to_s, 'HPXMLtoOpenStudio/resources') # Hack to run measures in the OS App since applied measures are copied off into a temporary directory
+  resources_path = File.join(OpenStudio::BCLMeasure::userMeasuresDir.to_s, 'HPXMLtoOpenStudio/resources')
+end
+require File.join(resources_path, 'location')
+require File.join(resources_path, 'meta_measure')
+require File.join(resources_path, 'weather')
+require 'pycall'
+require 'open3'
+
+# start the measure
+class HEScoreHPXML < OpenStudio::Measure::ModelMeasure
+  # human readable name
+  def name
+    # Measure name should be the title case of the class name.
+    return 'HEScore-HPXML'
+  end
+
+  # human readable description
+  def description
+    return 'Translates HPXML files to a Home Energy Score input json file'
+  end
+
+  # human readable description of modeling approach
+  def modeler_description
+    return ''
+  end
+
+  # define the arguments that the user will input
+  def arguments(model)
+    args = OpenStudio::Measure::OSArgumentVector.new
+
+    arg = OpenStudio::Ruleset::OSArgument.makeStringArgument('os_hescore_directory', false)
+    arg.setDisplayName('OpenStudio-HEScore Directory')
+    arg.setDescription(" ")
+    args << arg
+
+    return args
+  end
+
+  # define what happens when the measure is run
+  def run(model, runner, user_arguments)
+    super(model, runner, user_arguments)
+
+    # use the built-in error checking
+    if !runner.validateUserArguments(arguments(model), user_arguments)
+      return false
+    end
+
+    # assign the user inputs to variables
+    args = get_argument_values(runner, arguments(model), user_arguments)
+  
+    hpxml_path = File.expand_path('../in.xml') # this is the defaulted hpxml
+    runner.registerWarning("Running hescorehpxml measures (resstock)")
+    outfile = File.expand_path('../testingout.json')
+
+    # output = `hpxml2hescore #{hpxml_path} -o #{outfile} --resstock`
+    # puts("system result: #{output}")
+    # puts($?) unless output
+
+    stdout, stderr, status = Open3.capture2e("python3", "-m", "hescorehpxml", "\"#{hpxml_path}\"", "-o", "\"#{outfile}\"", "--resstock")
+    puts("stdout: #{stdout}")
+    puts("stderr: #{stderr}")
+    puts("status: #{status}")
+
+    runner.registerWarning("Ran translator with hpxml2hescore \"#{hpxml_path}\" -o \"#{outfile}\"--resstock")
+
+    files = Dir['/var/simdata/openstudio/run/*']
+    puts("----------/var/simdata/openstudio/run")
+    puts(files)
+
+    files = Dir['/var/simdata/openstudio/*']
+    puts("----------/var/simdata/openstudio ")
+    puts(files)
+
+    return true
+
+    ###############################################################
+
+    # assign the user inputs to variables
+    args = get_argument_values(runner, arguments(model), user_arguments)
+
+    measures_dir = File.absolute_path(File.join(File.dirname(__FILE__), '../../resources/hpxml-measures'))
+    arg_names = []
+
+
+# register the measure to be used by the application
+HEScoreHPXML.new.registerWithApplication
