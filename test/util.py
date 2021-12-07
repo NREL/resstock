020109af815ae7e259c7cd078aeb145846c373c6
Joe Robertson <joseph.robertson@nrel.gov>
2021-12-07 12:23:54 -0700

    Organize annual vs timeseries.

--- test/util.py
+++ test/util.py
@@ -3,9 +3,8 @@ import pandas as pd
 
 # BASELINE
 
-outdir = 'baseline'
-if not os.path.exists(outdir):
-  os.makedirs(outdir)
+if not os.path.exists('baseline'):
+  os.makedirs('baseline')
 
 df_national = pd.read_csv('project_national/national_baseline/results_csvs/results_up00.csv')
 df_national['building_id'] = df_national['building_id'].apply(lambda x: 'project_national-{}.osw'.format('%04d' % x))
@@ -35,13 +34,20 @@ for col in df.columns.values:
   elif col.startswith('qoi_report'):
     qoi_reports.append(col)
 
-# results_characteristics.csv
 results_characteristics = df[['OSW'] + build_existing_models]
 for col in results_characteristics.columns.values:
   results_characteristics = results_characteristics.rename(columns={col: col.replace('build_existing_model.', '')})
 
 results_characteristics = results_characteristics.set_index('OSW')
 results_characteristics = results_characteristics.reindex(sorted(results_characteristics), axis=1)
+
+# Annual
+
+outdir = 'baseline/annual'
+if not os.path.exists(outdir):
+  os.makedirs(outdir)
+
+# results_characteristics.csv
 results_characteristics.to_csv(os.path.join(outdir, 'results_characteristics.csv'))
 
 # results_output.csv
@@ -56,31 +62,45 @@ results_output = results_output.set_index('OSW')
 results_output = results_output.reindex(sorted(results_output), axis=1)
 results_output.to_csv(os.path.join(outdir, 'results_output.csv'))
 
-# results_timeseries.csv
+# Timeseries
+
+outdir = 'baseline/timeseries'
+if not os.path.exists(outdir):
+  os.makedirs(outdir)
+
 frames = []
 index_col = ['Time', 'TimeDST', 'TimeUTC']
 
-for dp in os.listdir('project_national/national_baseline/simulation_output/up00'):
-  df = pd.read_csv('project_national/national_baseline/simulation_output/up00/{}/run/enduse_timeseries.csv'.format(dp), index_col=index_col)
+for dp in os.listdir('project_national/national_upgrades/simulation_output/up00'):
+  df = pd.read_csv('project_national/national_upgrades/simulation_output/up00/{}/run/enduse_timeseries.csv'.format(dp), index_col=index_col)
   s = df.max() # FIXME
   df = pd.DataFrame([s.tolist()], columns=s.index)
   df['OSW'] = 'project_national-{}.osw'.format(dp[-4:])
   frames.append(df)
 
-for dp in os.listdir('project_testing/testing_baseline/simulation_output/up00'):
-  df = pd.read_csv('project_testing/testing_baseline/simulation_output/up00/{}/run/enduse_timeseries.csv'.format(dp), index_col=index_col)
+for dp in os.listdir('project_testing/testing_upgrades/simulation_output/up00'):
+  df = pd.read_csv('project_testing/testing_upgrades/simulation_output/up00/{}/run/enduse_timeseries.csv'.format(dp), index_col=index_col)
   s = df.max() # FIXME
   df = pd.DataFrame([s.tolist()], columns=s.index)
   df['OSW'] = 'project_testing-{}.osw'.format(dp[-4:])
   frames.append(df)
 
-results_timeseries = pd.concat(frames)
-results_timeseries = results_timeseries.set_index('OSW')
-results_timeseries.to_csv(os.path.join(outdir, 'results_timeseries.csv'))
+# results_characteristics.csv
+results_characteristics.to_csv(os.path.join(outdir, 'results_characteristics.csv'))
+
+# results_output.csv
+results_output = pd.concat(frames)
+results_output = results_output.set_index('OSW')
+results_output.to_csv(os.path.join(outdir, 'results_output.csv'))
 
 # UPGRADES
 
-outdir = 'upgrades'
+if not os.path.exists('upgrades'):
+  os.makedirs('upgrades')
+
+# Annual
+
+outdir = 'upgrades/annual'
 if not os.path.exists(outdir):
   os.makedirs(outdir)
 
