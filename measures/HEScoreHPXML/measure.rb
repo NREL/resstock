3f20fcb4b616cb024c452cc427fc153d9401fec3
aspeake1 <andrew.speake@nrel.gov>
2022-01-04 16:01:40 -0700

    clean up HES translator measure

--- measures/HEScoreHPXML/measure.rb
+++ measures/HEScoreHPXML/measure.rb
@@ -4,17 +4,7 @@
 # http://nrel.github.io/OpenStudio-user-documentation/measures/measure_writing_guide/
 
 require 'openstudio'
-if File.exist? File.absolute_path(File.join(File.dirname(__FILE__), '../../lib/resources/hpxml-measures/HPXMLtoOpenStudio/resources')) # Hack to run ResStock on AWS
-  resources_path = File.absolute_path(File.join(File.dirname(__FILE__), '../../lib/resources/hpxml-measures/HPXMLtoOpenStudio/resources'))
-elsif File.exist? File.absolute_path(File.join(File.dirname(__FILE__), '../../resources/hpxml-measures/HPXMLtoOpenStudio/resources')) # Hack to run ResStock unit tests locally
-  resources_path = File.absolute_path(File.join(File.dirname(__FILE__), '../../resources/hpxml-measures/HPXMLtoOpenStudio/resources'))
-elsif File.exist? File.join(OpenStudio::BCLMeasure::userMeasuresDir.to_s, 'HPXMLtoOpenStudio/resources') # Hack to run measures in the OS App since applied measures are copied off into a temporary directory
-  resources_path = File.join(OpenStudio::BCLMeasure::userMeasuresDir.to_s, 'HPXMLtoOpenStudio/resources')
-end
-require File.join(resources_path, 'location')
-require File.join(resources_path, 'meta_measure')
-require File.join(resources_path, 'weather')
-require 'pycall'
+require 'open3'
 
 # start the measure
 class HEScoreHPXML < OpenStudio::Measure::ModelMeasure
@@ -38,13 +28,19 @@ class HEScoreHPXML < OpenStudio::Measure::ModelMeasure
   def run(model, runner, user_arguments)
     super(model, runner, user_arguments)
 
-    hpxml_path = File.expand_path('../in.xml') # this is the defaulted hpxml
+    hpxml_path = File.expand_path('../existing.xml')
     outfile = File.expand_path('../hes.json')
 
-    runner.registerWarning('Translating xml to HES json')
+    runner.registerInfo('Translating xml to HES json')
     command = "hpxml2hescore #{hpxml_path} -o #{outfile} --resstock"
-    system(command)
-    runner.registerWarning("Translated xml to HES json, output #{outfile}")
+    stdout, stderr, status = Open3.capture3(command)
+
+    if not status.success?
+      runner.registerError(stderr)
+      return false
+    end
+
+    runner.registerInfo("Translated xml to HES json, output #{outfile}")
 
     return true
   end
