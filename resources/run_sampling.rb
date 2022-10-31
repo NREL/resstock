7d7fd293452592252c7ca4f2c9a7a28b763fe68a
Scott Horowitz <scott.horowitz@nrel.gov>
2022-10-31 11:05:05 -0600

    Fixes using OS CLI w/ run_sampling.rb.

--- resources/run_sampling.rb
+++ resources/run_sampling.rb
@@ -306,38 +306,36 @@ class RunSampling
   end
 end
 
-if __FILE__ == $PROGRAM_NAME
-  options = {}
-  OptionParser.new do |opts|
-    opts.banner = "Usage: #{File.basename(__FILE__)} -p project_name -n num_datapoints -o outfile\n e.g., #{File.basename(__FILE__)} -p project_national -n 10000 -o buildstock.csv"
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{File.basename(__FILE__)} -p project_name -n num_datapoints -o outfile\n e.g., #{File.basename(__FILE__)} -p project_national -n 10000 -o buildstock.csv"
 
-    opts.on('-p', '--project <STRING>', 'Project Name') do |t|
-      options[:project] = t
-    end
-
-    opts.on('-n', '--num-datapoints <INTEGER>', 'Number of datapoints') do |t|
-      options[:numdps] = t.to_i
-    end
-
-    opts.on('-o', '--output <STRING>', 'Output file name') do |t|
-      options[:outfile] = t
-    end
-
-    opts.on_tail('-h', '--help', 'Display help') do
-      puts opts
-      exit
-    end
-  end.parse!
-
-  if (not options[:project]) || (not options[:numdps]) || (not options[:outfile])
-    fail "ERROR: All 3 arguments are required. Call #{File.basename(__FILE__)} -h for usage."
+  opts.on('-p', '--project <STRING>', 'Project Name') do |t|
+    options[:project] = t
   end
 
-  r = RunSampling.new
+  opts.on('-n', '--num-datapoints <INTEGER>', 'Number of datapoints') do |t|
+    options[:numdps] = t.to_i
+  end
 
-  t0 = Time.now
-  r.run(options[:project], options[:numdps], options[:outfile])
-  t1 = Time.now
+  opts.on('-o', '--output <STRING>', 'Output file name') do |t|
+    options[:outfile] = t
+  end
 
-  puts "Sampling took: #{((t1 - t0) / 60.0).round(1)} minutes."
+  opts.on_tail('-h', '--help', 'Display help') do
+    puts opts
+    exit!
+  end
+end.parse!
+
+if (not options[:project]) || (not options[:numdps]) || (not options[:outfile])
+  fail "ERROR: All 3 arguments are required. Call #{File.basename(__FILE__)} -h for usage."
 end
+
+r = RunSampling.new
+
+t0 = Time.now
+r.run(options[:project], options[:numdps], options[:outfile])
+t1 = Time.now
+
+puts "Sampling took: #{((t1 - t0) / 60.0).round(1)} minutes."
