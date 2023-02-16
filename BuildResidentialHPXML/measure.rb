56b4f87c9d7849288253e02a2dfafb08e59ed0eb
Philip R. White <philip.white@utexas.edu>
2023-02-16 15:32:29 -0700

    Squashed 'resources/hpxml-measures/' changes from b9fb3669d8..d35a81fc51
    d35a81fc51 Merge pull request #1289 from NREL/hpwh_operating_mode
    3eebbf6f8e Replaces `WaterHeatingSystem/extension/OperatingMode` with `WaterHeatingSystem/HPWHOperatingMode` for heat pump water heaters.
    335139effd Merge pull request #1288 from NREL/hpxml_merged_xsd
    a7cf0800e1 Switch to using the HPXMLMerged.xsd (renamed to HPXML.xsd so we and other software tools don't have to update code).
    b094d25026 Merge branch 'master' of https://github.com/NREL/OpenStudio-HPXML
    bbf7f8eb2e Reduce AFUE/efficiency warnings from 60% to 50%. (See https://www.homedepot.com/p/Williams-45-000-BTU-Natural-Gas-Floor-Heater-4505622A/311802910 for example.)
    21d338c4af Merge pull request #1287 from NREL/infil_refactor
    7d1f27063d Update xml
    eba00b58a2 Small infiltration refactor.
    7cf7f19233 NumberofUnits->Count
    
    git-subtree-dir: resources/hpxml-measures
    git-subtree-split: d35a81fc5195c26f9cb492775919cf334f37bd25

--- BuildResidentialHPXML/measure.rb
+++ BuildResidentialHPXML/measure.rb
@@ -1839,12 +1839,12 @@ class BuildResidentialHPXML < OpenStudio::Measure::ModelMeasure
     args << arg
 
     water_heater_operating_mode_choices = OpenStudio::StringVector.new
-    water_heater_operating_mode_choices << HPXML::WaterHeaterOperatingModeStandard
+    water_heater_operating_mode_choices << HPXML::WaterHeaterOperatingModeHybridAuto
     water_heater_operating_mode_choices << HPXML::WaterHeaterOperatingModeHeatPumpOnly
 
     arg = OpenStudio::Measure::OSArgument::makeChoiceArgument('water_heater_operating_mode', water_heater_operating_mode_choices, false)
     arg.setDisplayName('Water Heater: Operating Mode')
-    arg.setDescription("The water heater operating mode. The '#{HPXML::WaterHeaterOperatingModeHeatPumpOnly}' option only uses the heat pump, while '#{HPXML::WaterHeaterOperatingModeStandard}' allows the backup electric resistance to come on in high demand situations. This is ignored if a scheduled operating mode type is selected. Applies only to #{HPXML::WaterHeaterTypeHeatPump}. If not provided, the OS-HPXML default is used.")
+    arg.setDescription("The water heater operating mode. The '#{HPXML::WaterHeaterOperatingModeHeatPumpOnly}' option only uses the heat pump, while '#{HPXML::WaterHeaterOperatingModeHybridAuto}' allows the backup electric resistance to come on in high demand situations. This is ignored if a scheduled operating mode type is selected. Applies only to #{HPXML::WaterHeaterTypeHeatPump}. If not provided, the OS-HPXML default is used.")
     args << arg
 
     hot_water_distribution_system_type_choices = OpenStudio::StringVector.new
--- HPXMLtoOpenStudio/resources/hpxml_defaults.rb
+++ HPXMLtoOpenStudio/resources/hpxml_defaults.rb
@@ -48,7 +48,7 @@ class HPXMLDefaults
     apply_hvac(runner, hpxml, weather, convert_shared_systems)
     apply_hvac_control(hpxml, schedules_file)
     apply_hvac_distribution(hpxml, ncfl, ncfl_ag)
-    apply_ventilation_fans(hpxml, infil_measurement, weather, cfa, nbeds)
+    apply_ventilation_fans(hpxml, weather, cfa, nbeds)
     apply_water_heaters(hpxml, nbeds, eri_version, schedules_file)
     apply_hot_water_distribution(hpxml, cfa, ncfl, has_uncond_bsmnt)
     apply_water_fixtures(hpxml, schedules_file)
@@ -1541,7 +1541,7 @@ class HPXMLDefaults
     end
   end
 
-  def self.apply_ventilation_fans(hpxml, infil_measurement, weather, cfa, nbeds)
+  def self.apply_ventilation_fans(hpxml, weather, cfa, nbeds)
     # Default mech vent systems
     hpxml.ventilation_fans.each do |vent_fan|
       next unless vent_fan.used_for_whole_building_ventilation
@@ -1559,7 +1559,7 @@ class HPXMLDefaults
           fail 'Defaulting flow rates for multiple mechanical ventilation systems is currently not supported.'
         end
 
-        vent_fan.rated_flow_rate = Airflow.get_default_mech_vent_flow_rate(hpxml, vent_fan, [infil_measurement], weather, cfa, nbeds).round(1)
+        vent_fan.rated_flow_rate = Airflow.get_default_mech_vent_flow_rate(hpxml, vent_fan, weather, cfa, nbeds).round(1)
         vent_fan.rated_flow_rate_isdefaulted = true
       end
       if vent_fan.fan_power.nil?
@@ -1692,7 +1692,7 @@ class HPXMLDefaults
       if (water_heating_system.water_heater_type == HPXML::WaterHeaterTypeHeatPump)
         schedules_file_includes_water_heater_operating_mode = (schedules_file.nil? ? false : schedules_file.includes_col_name(SchedulesFile::ColumnWaterHeaterOperatingMode))
         if water_heating_system.operating_mode.nil? && !schedules_file_includes_water_heater_operating_mode
-          water_heating_system.operating_mode = HPXML::WaterHeaterOperatingModeStandard
+          water_heating_system.operating_mode = HPXML::WaterHeaterOperatingModeHybridAuto
           water_heating_system.operating_mode_isdefaulted = true
         end
       end
