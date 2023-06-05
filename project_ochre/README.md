6d80438446abe1d0308d9d4cae29418767afcf7c
Jeff Maguire <Jeff.Maguire@nrel.gov>
2023-06-05 12:18:26 -0600

    Copy of project_testing for customized testing of features we want to support in ochre (project_ochre)

---
+++ project_ochre/README.md
@@ -0,0 +1,3 @@
+# Testing Project Folder
+
+This folder contains the ResStock inputs to test our OpenStudio measures workflow. The main inputs that characterize the building stock is the housing characteristics in the `housing_characteristics/` directory.
---
+++ project_ochre/tests/test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative '../../resources/hpxml-measures/HPXMLtoOpenStudio/resources/minitest_helper'
+require_relative '../../test/integrity_checks'
+
+class TestProjectTesting < MiniTest::Test
+  def test_housing_characteristics
+    begin
+      project_dir_name = File.basename(File.dirname(File.dirname(__FILE__)))
+      integrity_check(project_dir_name)
+      integrity_check_options_lookup_tsv(project_dir_name)
+    rescue Exception => e
+      flunk e
+
+      # Need a backtrace? Uncomment below
+      # flunk "#{e}\n#{e.backtrace.join('\n')}"
+    end
+  end
+end
