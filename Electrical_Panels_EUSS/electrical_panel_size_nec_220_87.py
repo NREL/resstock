1e7b87ced478032aa7b3d55a5b9363a8922e8c7c
juliaehlers <135869953+juliaehlers@users.noreply.github.com>
2023-07-06 16:41:59 -0600

    Create electrical_panel_size_nec_220_87.py
    Created file, added brief description of file intentions/purpose

---
+++ Electrical_Panels_EUSS/electrical_panel_size_nec_220_87.py
@@ -0,0 +1,6 @@
+"""
+NEC 220.87 - Determining Existing Loads
+  - Uses maximum demand data from a year to size service/electrical panel
+  - Takes maximum demand (plus new load if applicable) times demand factor 1.25 to find kVA of current load
+  - Divide kVA value by 240 V to find panel amperage
+"""
