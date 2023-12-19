a6f6363845b1ede8795a294bbf387b6c069c5b6d
Joe Robertson <joseph.robertson@nrel.gov>
2023-12-19 11:09:19 -0700

    Skip the missing design conditions warning in log.

--- resources/buildstock.rb
+++ resources/buildstock.rb
@@ -461,9 +461,10 @@ class RunOSWs
       next if line.include? 'Cannot find current Workflow Step'
       next if line.include? 'Data will be treated as typical (TMY)'
       next if line.include? 'WorkflowStepResult value called with undefined stepResult'
+      next if line.include?("Object of type 'Schedule:Constant' and named 'Always") && line.include?('points to an object named') && line.include?('but that object cannot be located')
+      next if line.include? 'Appears there are no design condition fields in the EPW file'
       next if line.include? 'No valid weather file defined in either the osm or osw.'
       next if line.include? 'EPW file not found'
-      next if line.include?("Object of type 'Schedule:Constant' and named 'Always") && line.include?('points to an object named') && line.include?('but that object cannot be located')
       next if line.include? "'UseWeatherFile' is selected in YearDescription, but there are no weather file set for the model."
       next if line.include? 'not within the expected limits' # Ignore EpwFile warnings
       next if line.include? 'Unable to find sql file at'
