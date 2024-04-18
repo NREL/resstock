a60145e33ded473f127a3881ce769b36766444b1
jmaguire1 <Jeff.Maguire@nrel.gov>
2024-04-18 15:02:21 -0600

    Add 120V HPWH options, triggered by high UEF (COP is overwritten later)

--- resources/hpxml-measures/HPXMLtoOpenStudio/resources/waterheater.rb
+++ resources/hpxml-measures/HPXMLtoOpenStudio/resources/waterheater.rb
@@ -62,8 +62,13 @@ class Waterheater
 
   def self.apply_heatpump(model, runner, loc_space, loc_schedule, elevation, water_heating_system, ec_adj, solar_thermal_system, conditioned_zone, eri_version, schedules_file, unavailable_periods, unit_multiplier, nbeds)
     # TODO: flag for testing, need to add whether this is 120 V or 240 V, shared or dedicated circuit, to the HPXML files. Extension elements?
-    hpwh_120V = true
-    hpwh_120V_type = 'shared' # 'dedicated'
+    
+    hpwh_120V = false
+    hpwh_120V_type = 'dedicated'
+    if water_heating_system.uniform_energy_factor > 5.0
+      hpwh_120V = true
+      hpwh_120V_type = 'shared' # 'dedicated'
+    end
 
     obj_name_hpwh = Constants.ObjectNameWaterHeater
     solar_fraction = get_water_heater_solar_fraction(water_heating_system, solar_thermal_system)
