85d948ce6aa4f1766925a988478f3f3d02df0f84
Yingli <ylou2@nrel.gov>
2024-10-28 11:34:42 -0600

    first commit

--- measures/ResStockArguments/resources/electrical_panel.rb
+++ measures/ResStockArguments/resources/electrical_panel.rb
@@ -23,6 +23,19 @@ class RatedCapacityGenerator
     return capacity_bin, capacity_value
   end
 
+  def assign_breaker_spaces_headroom(args:)
+    # initialize a random number generator
+    prng = Random.new(args[:building_id])
+
+    # load probability distribution csv
+    breaker_spaces_headroom_prob_map = read_breaker_spaces_headroom_probs()
+
+    # assign breaker space headroom number
+    breaker_spaces_headroom = sample_breaker_spaces_headroom(prng, breaker_spaces_headroom_prob_map, args)
+
+    return breaker_spaces_headroom
+  end
+
   def sample_rated_capacity_bin(prng, rated_capacity_map, args)
     # emulate Geometry Building Type RECS
     geometry_building_type_recs = convert_building_type(args[:geometry_unit_type], args[:geometry_building_num_units])
@@ -65,6 +78,40 @@ class RatedCapacityGenerator
     return capacity_bins[index]
   end
 
+  def sample_breaker_spaces_headroom(prng, breaker_spaces_headroom_prob_map, args)
+    # get panel capacity bin 
+    cap_bin, cap_val = assign_rated_capacity(args: args)
+
+    # emulate HVAC Cooling Type 
+    hvac_cooling_type = convert_cooling_type(args[:cooling_system_type], args[:heat_pump_type])
+    # emulate HVAC Heating Type 
+    hvac_heating_type = convert_heating_type(args[:heating_system_type], args[:heat_pump_type])
+
+    # simplify appliance presence and fuel
+    clothes_dryer = convert_fuel_and_presence(args[:clothes_dryer_present], args[:clothes_dryer_fuel_type])
+    cooking_range = convert_fuel_and_presence(args[:cooking_range_oven_present], args[:cooking_range_fuel_type])
+    water_heater_fuel_type = simplify_fuel_type(args[:water_heater_fuel_type])
+    heating_fuel_type = simplify_fuel_type(args[:heating_system_fuel])
+    ev_charger_present = FALSE
+
+    lookup_array = [
+      hvac_cooling_type,
+      hvac_heating_type,
+      heating_fuel_type,
+      water_heater_fuel_type,
+      clothes_dryer,
+      cooking_range,
+      args[:pv_system_present].to_s,
+      ev_charger_present.to_s,
+      cap_bin,
+    ]
+  end
+    breaker_spaces_headroom = get_row_headers_breaker_spaces_headroom(breaker_spaces_headroom_prob_map, lookup_array)
+    row_probability = get_row_probability_breaker_spaces_headroom(breaker_spaces_headroom_prob_map, lookup_array)
+    index = weighted_random(prng, row_probability)
+    return breaker_spaces_headroom[index]
+  end
+
   def convert_capacity_bin_to_value(capacity_bin, heating_system_fuel, geometry_unit_cfa_bin)
     if capacity_bin == '<100'
       if heating_system_fuel == HPXML::FuelTypeElectricity
@@ -100,9 +147,22 @@ class RatedCapacityGenerator
     return prob_table
   end
 
+  def read_breaker_spaces_headroom_probs()
+    filename = 'electrical_panel_breaker_space.csv'
+    file = File.absolute_path(File.join(File.dirname(__FILE__), 'electrical_panel_resources', filename))
+    prob_table = CSV.read(file)
+    return prob_table
+  end
+
   def get_row_headers(prob_table, lookup_array)
     len = lookup_array.length()
     row_headers = prob_table[0][len..len + 7]
+    return row_headers
+  end
+
+  def get_row_headers_breaker_spaces_headroom(prob_table, lookup_array)
+    len = lookup_array.length()
+    row_headers = prob_table[0][len..len + 32]
     return row_headers
   end
 
@@ -117,6 +177,21 @@ class RatedCapacityGenerator
 
     if row_probability.length() != 7
       @runner.registerError("RatedCapacityGenerator cannot find row_probability for keys: #{lookup_array}")
+    end
+    return row_probability
+  end
+
+  def get_row_probability_breaker_spaces_headroom(prob_table, lookup_array)
+    len = lookup_array.length()
+    row_probability = []
+    prob_table.each do |row|
+      next if row[0..len - 1] != lookup_array
+
+      row_probability = row[len..len + 32].map(&:to_f)
+    end
+
+    if row_probability.length() != 32
+      @runner.registerError("BreakerSpacesHeadroomGenerator cannot find row_probability for keys: #{lookup_array}")
     end
     return row_probability
   end
@@ -161,6 +236,14 @@ class RatedCapacityGenerator
       return 'none'
     else
       @runner.registerError("RatedCapacityGenerator cannot determine cooling type based on '#{args[:system_cooling_type]}' and '#{args[:heat_pump_type]}'.")
+    end
+  end
+
+  def convert_heating_type(heat_pump_type)
+    if [HPXML::HVACTypeHeatPumpAirToAir, HPXML::HVACTypeHeatPumpGroundToAir].include?(heat_pump_type)
+      return 'ASHP or GHP'
+    else
+      return 'not ASHP or GHP'
     end
   end
 
