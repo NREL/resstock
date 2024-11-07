71f0c36243e66d77a1e54317cd672078db2ece65
Yingli Lou <ylou2@kl2.head.cm.kestrel.hpc.nrel.gov>
2024-11-06 17:16:31 -0700

    final run

--- technoeconomic_analysis/Postprocessing/model_extraction.py
+++ technoeconomic_analysis/Postprocessing/model_extraction.py
@@ -1,10 +1,11 @@
 import os
 import os.path
 
-file_path = '/kfs2/projects/panels/ochre/resstock_model/30k' 
-upgrade_list = list(range(1, 20))
+file_path = '/kfs2/projects/panels/ochre/resstock_model/30k_20240918' 
+#upgrade_list = list(range(1, 20))
+upgrade_list = [1,2,3,4,5,6,7,9,10,11,13,14,16,17,18,19]
 for upgrade in upgrade_list:
-    folder = f'/kfs2/projects/panels/ochre/resstock_model/30k/upgrade{upgrade}/results/simulation_output/up01'
+    folder = f'{file_path}/upgrade{upgrade}/results/simulation_output/up01'
     build_list = [name for name in os.listdir(folder) if os.path.isdir(os.path.join(folder, name))]
     for build in build_list:
         xml = f'{folder}/{build}/run/in.xml'
